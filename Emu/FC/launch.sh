40c550b34461bddd85f4a2d67495b255806b1d69
jimgraygit <85693713+jimgraygit@users.noreply.github.com>
2022-01-06 16:42:37 -0500

    Pre-Release Testing

--- Emu/FC/launch.sh
+++ Emu/FC/launch.sh
@@ -3,4 +3,4 @@ echo $0 $*
 progdir=`dirname "$0"`
 homedir=`dirname "$1"`
 cd /mnt/SDCARD/RetroArch/
-HOME=/mnt/SDCARD/RetroArch/ $progdir/../../RetroArch/retroarch -v -L $progdir/fceumm_libretro.so "$1"
+HOME=/mnt/SDCARD/RetroArch/ $progdir/../../RetroArch/retroarch -v -L $progdir/quicknes_libretro.so "$1"
--- Emu/SEGACD/launch.sh
+++ Emu/SEGACD/launch.sh
@@ -2,5 +2,6 @@
 echo $0 $*
 progdir=`dirname "$0"`
 homedir=`dirname "$1"`
+./cpufreq.sh
 cd /mnt/SDCARD/RetroArch/
-HOME=/mnt/SDCARD/RetroArch/ $progdir/../../RetroArch/retroarch -v -L $progdir/genesis_plus_gx_libretro.so "$1"
+HOME=/mnt/SDCARD/RetroArch/ $progdir/../../RetroArch/retroarch -v -L $progdir/picodrive_libretro.so "$1"
