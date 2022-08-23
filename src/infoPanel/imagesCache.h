e3c944f848ba77431a22a205f3ae3aedebff0407
Sergey G <introkun@gmail.com>
2022-08-24 00:20:00 +0300

    Add tests with GTest for infoPanel (#101)
    * temp commit
    
    * * removed overriding of CFLAGS
    * moved compilation of imagesCache out of recipes.mk
    
    * fixed review note
    * moved src/test to test
    
    * * suppressed build messages for test
    
    * * explicit declaration of c/cpp standards
    * fixed includes for test
    
    * * set default c standard explicitly
    
    * * added test to GH actions
    
    * * added gtest to github actions
    
    * * get rid of sudo in actions
    
    * * fix build
    
    * * switch off test in action
    
    * * renamed test module to more abstract name
    * enabled test at CI
    
    * * fix CI
    
    * * fix test CI
    
    * * fix test CI
    
    * * fix CI
    
    * * fix CI
    
    * * fix CI
    
    * * fix CI
    
    * * fix CI
    
    * * fix CI
    
    * * moved test to separate GH action
    
    * * fixed test
    
    * * fix test
    
    * * fix test
    
    * * fix test
    
    * * fix test
    
    * * fix test
    
    * * fixed typo

--- src/infoPanel/imagesCache.h
+++ src/infoPanel/imagesCache.h
@@ -1,10 +1,18 @@
 #ifndef IMAGES_CACHE_H__
 #define IMAGES_CACHE_H__
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 struct SDL_Surface;
 
 char* drawImageByIndex(const int index, const int image_index, char **images_paths,
     const int images_paths_count, SDL_Surface *screen, bool *cache_used);
 void cleanImagesCache();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // IMAGES_CACHE_H__
