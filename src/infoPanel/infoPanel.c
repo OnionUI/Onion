99758edb10105bd5274040c8f6c901b2b83e98d3
introkun <introkun@gmail.com>
2022-08-26 16:58:24 +0200

    * added lang for NEXT button * added titles for images from JSON for infoPanel * added theming for infoPanel

--- src/infoPanel/infoPanel.c
+++ src/infoPanel/infoPanel.c
@@ -9,6 +9,7 @@
 #include "system/battery.h"
 #include "system/keymap_sw.h"
 #include "system/settings.h"
+#include "system/lang.h"
 #include "theme/theme.h"
 #include "theme/background.h"
 #include "imagesCache.h"
@@ -18,10 +19,12 @@
 #define FRAMES_PER_SECOND 60
 
 static char **g_images_paths;
+static char **g_images_titles;
 static int g_images_paths_count = 0;
-static int g_image_index = 0;
+static int g_image_index = -1;
 
-static bool loadImagesPathsFromJson(const char* config_path, char ***images_paths, int *images_paths_count)
+static bool loadImagesPathsFromJson(const char* config_path, char ***images_paths, int *images_paths_count,
+	char ***images_titles)
 {
 	const char *json_str = NULL;
 
@@ -35,15 +38,22 @@ static bool loadImagesPathsFromJson(const char* config_path, char ***images_path
 	cJSON* json_images_array = cJSON_GetObjectItem(json_root, "images");
 	*images_paths_count = cJSON_GetArraySize(json_images_array);
 	*images_paths = (char**)malloc(*images_paths_count * sizeof(char*));
+	*images_titles = (char**)malloc(*images_paths_count * sizeof(char*));
 
 	for (int i = 0; i < *images_paths_count; i++)
 	{
 		(*images_paths)[i] = (char*)malloc(STR_MAX * sizeof(char));
+		static const int g_title_max_length = 50;
+		(*images_titles)[i] = (char*)malloc(g_title_max_length * sizeof(char));
 
-		cJSON* json_image_path_item = cJSON_GetArrayItem(json_images_array, i);
-		cJSON* json_image_path = cJSON_GetObjectItem(json_image_path_item, "path");
+		cJSON* json_image_item = cJSON_GetArrayItem(json_images_array, i);
+		cJSON* json_image_path = cJSON_GetObjectItem(json_image_item, "path");
 		char* image_path = cJSON_GetStringValue(json_image_path);
-		strcpy((*images_paths)[i], image_path);
+		strncpy((*images_paths)[i], image_path, STR_MAX);
+
+		cJSON* json_image_title = cJSON_GetObjectItem(json_image_item, "title");
+		char* image_title = cJSON_GetStringValue(json_image_title);
+		strncpy((*images_titles)[i], image_title, g_title_max_length);
 	}
 
 	cJSON_free(json_root);
@@ -53,9 +63,7 @@ static bool loadImagesPathsFromJson(const char* config_path, char ***images_path
 
 static void drawInfoPanel(SDL_Surface *screen, SDL_Surface *video, const char *title_str, char *message_str)
 {
-	SDL_BlitSurface(theme_background(), NULL, screen, NULL);
-	SDL_BlitSurface(screen, NULL, video, NULL); 
-	SDL_Flip(video);
+	theme_backgroundBlit(video);
 
 	bool has_title = strlen(title_str) > 0;
 	bool has_message = strlen(message_str) > 0;
@@ -64,19 +72,15 @@ static void drawInfoPanel(SDL_Surface *screen, SDL_Surface *video, const char *t
 	SDL_Rect message_rect = {320, 240};
 
 	theme_renderHeader(screen, has_title ? title_str : NULL, !has_title);
-	theme_renderFooter(screen);
-	theme_renderHeaderBattery(screen, battery_getPercentage());
 
 	if (has_message) {
-		char *str = str_replace(message_str, "\\n", "\n");
+		const char *str = str_replace(message_str, "\\n", "\n");
 		message = theme_textboxSurface(str, resource_getFont(TITLE), theme()->grid.color, ALIGN_CENTER);
 		message_rect.x -= message->w / 2;
 		message_rect.y -= message->h / 2;
 		SDL_BlitSurface(message, NULL, screen, &message_rect);
 		SDL_FreeSurface(message);
 	}
-
-	resources_free();
 }
 
 static void drawImage(const char *image_path, SDL_Surface *screen)
@@ -96,6 +100,12 @@ static void sdlQuit(SDL_Surface *screen, SDL_Surface *video)
     SDL_Quit();
 }
 
+static const char *getFilename(const char *full_path) {
+    const char *slash = strrchr(full_path, '/');
+    if(!slash || slash == full_path) return "";
+    return slash + 1;
+}
+
 int main(int argc, char *argv[])
 {
 	char title_str[STR_MAX] = "";
@@ -104,6 +114,8 @@ int main(int argc, char *argv[])
 	char images_json_path[STR_MAX] = "";
 	char images_dir_path[STR_MAX] = "";
 	bool wait_confirm = true;
+	bool show_theme_controls = false;
+	bool info_panel_mode = false;
 
 	for (int i = 1; i < argc; i++) {
 		if (argv[i][0] == '-') {
@@ -117,6 +129,8 @@ int main(int argc, char *argv[])
 				strncpy(images_json_path, argv[++i], STR_MAX-1);
 			else if (strcmp(argv[i], "-d") == 0 || strcmp(argv[i], "--directory") == 0)
 				strncpy(images_dir_path, argv[++i], STR_MAX-1);
+			else if (strcmp(argv[i], "-s") == 0 || strcmp(argv[i], "--show-theme-controls") == 0)
+				show_theme_controls = true;
 			else if (strcmp(argv[i], "-a") == 0 || strcmp(argv[i], "--auto") == 0)
 				wait_confirm = false;
 		}
@@ -138,14 +152,16 @@ int main(int argc, char *argv[])
 	bool cache_used = false;
 	if (exists(image_path)) {
 		g_images_paths_count = 1;
+		g_image_index = 0;
 		drawImage(image_path, screen);
 	}
 	else if(exists(images_json_path))
 	{
-		if (loadImagesPathsFromJson(images_json_path, &g_images_paths, &g_images_paths_count))
+		if (loadImagesPathsFromJson(images_json_path, &g_images_paths, &g_images_paths_count, &g_images_titles))
 		{
 			if (g_images_paths_count > 0)
 			{
+				g_image_index = 0;
 				drawImageByIndex(0, g_image_index, g_images_paths, g_images_paths_count, screen, &cache_used);
 			}
 		}
@@ -160,6 +176,7 @@ int main(int argc, char *argv[])
 		{
 			if (g_images_paths_count > 0)
 			{
+				g_image_index = 0;
 				drawImageByIndex(0, g_image_index, g_images_paths, g_images_paths_count, screen, &cache_used);
 			}
 		}
@@ -170,7 +187,8 @@ int main(int argc, char *argv[])
 		}
 	}
 	else {
-		drawInfoPanel(screen, video, title_str, message_str);
+		info_panel_mode = true;
+		show_theme_controls = true;
 	}
 
 	SDL_BlitSurface(screen, NULL, video, NULL);
@@ -210,7 +228,8 @@ int main(int argc, char *argv[])
 				{
 					continue;
 				}
-				if ((navigating_forward && g_image_index == g_images_paths_count - 1) // exit after last image
+				if (info_panel_mode // drawing info panel
+					|| (navigating_forward && g_image_index == g_images_paths_count - 1) // exit after last image
 					|| (!navigating_forward && g_image_index == 0)) // or when navigating backwards from the first image
 				{
 					quit = true;
@@ -226,7 +245,7 @@ int main(int argc, char *argv[])
 			}
 		}
 
-		if (all_changed) {
+		if (show_theme_controls && all_changed) {
 			header_changed = true;
 			footer_changed = true;
 			battery_changed = true;
@@ -235,36 +254,67 @@ int main(int argc, char *argv[])
 		if (quit)
 			break;
 
-		if (battery_hasChanged(ticks, &battery_percentage))
+		if (show_theme_controls) {
+			if (battery_hasChanged(ticks, &battery_percentage))
 			battery_changed = true;
 		
-		if (acc_ticks >= time_step) {
-			if (header_changed || battery_changed)
-				theme_renderHeader(screen, "infoPanel", false);
-			
-			if (footer_changed) {
-				theme_renderFooter(screen);
-				theme_renderStandardHint(screen, lang_get(LANG_SELECT), lang_get(LANG_BACK));
+			if (acc_ticks >= time_step) {
+				if (header_changed || battery_changed) {
+					if (info_panel_mode) {
+						drawInfoPanel(screen, video, title_str, message_str);
+					}
+					else if (g_images_titles) {
+						theme_renderHeader(screen, g_images_titles[g_image_index], false);
+					}
+					else {
+						const char *current_image_path = image_path;
+						if (g_images_paths_count > 0 && g_images_paths && g_image_index >= 0) {
+							current_image_path = g_images_paths[g_image_index];
+						}
+						theme_renderHeader(screen, getFilename(current_image_path), false);
+					}
+				}
+				
+				if (footer_changed) {
+					theme_renderFooter(screen);
+					const char * a_btn_text = lang_get(LANG_NEXT);
+					const char * b_btn_text = lang_get(LANG_BACK);
+					if (info_panel_mode || g_images_paths_count == 1) {
+						a_btn_text = lang_get(LANG_OK);
+						b_btn_text = NULL;
+					}
+					else if (g_image_index == g_images_paths_count - 1) {
+						a_btn_text = lang_get(LANG_EXIT);
+					}
+					else if (g_image_index == 0) {
+						b_btn_text = lang_get(LANG_EXIT);
+					}
+					theme_renderStandardHint(screen, a_btn_text, b_btn_text);
+				}
+
+				if (footer_changed && !info_panel_mode && g_images_paths_count > 1)
+					theme_renderFooterStatus(screen, g_image_index + 1, g_images_paths_count);
+
+				if (header_changed || battery_changed)
+					theme_renderHeaderBattery(screen, battery_percentage);
+
+				if (header_changed || footer_changed || battery_changed) {
+					SDL_BlitSurface(screen, NULL, video, NULL); 
+					SDL_Flip(video);
+				}
+
+				header_changed = false;
+				footer_changed = false;
+				battery_changed = false;
+				all_changed = false;
+
+				acc_ticks -= time_step;
 			}
-
-			if (footer_changed)
-				theme_renderFooterStatus(screen, g_image_index + 1, g_images_paths_count);
-
-			if (header_changed || battery_changed)
-				theme_renderHeaderBattery(screen, battery_percentage);
-
-			if (header_changed || footer_changed || battery_changed) {
-				SDL_BlitSurface(screen, NULL, video, NULL); 
-				SDL_Flip(video);
-			}
-
-			header_changed = false;
-			footer_changed = false;
-			battery_changed = false;
-			all_changed = false;
-
-			acc_ticks -= time_step;
 		}
+		else {
+			SDL_BlitSurface(screen, NULL, video, NULL); 
+			SDL_Flip(video);
+		}		
 	}
 
 	if (g_images_paths != NULL)
@@ -272,6 +322,9 @@ int main(int argc, char *argv[])
 		for (int i = 0; i < g_images_paths_count; i++)
         	free(g_images_paths[i]);
     	free(g_images_paths);
+		for (int i = 0; i < g_images_paths_count; i++)
+        	free(g_images_titles[i]);
+		free(g_images_titles);
 	}
 
 	if (!wait_confirm)
