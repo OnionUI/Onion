8d92937652ae5524d714400088976784533e198b
Schmurtz <schmurtz@gmail.com>
2022-09-22 10:33:51 +0200

    Fix Pico-8 glitching backgrounds when not viewed fullscreen (#140)
    * Fix Pico-8 glitching backgrounds when not viewed fullscreen
    
    Issue and fix on Fake-08 repo :
    https://github.com/jtothebell/fake-08/issues/167
    
    Issue declared in Onion :
    https://github.com/OnionUI/Onion/issues/138
    
    * Retroarch config : better refresh rate of FPS , no quit on close content
    
    Small changes :
    - FPS counter is refreshed every 20 frames
    - no quit on close content : allows for example to save you RA global config, still possible to quit directly from the menu

--- static/packages/Emu/PICO-8 (Fake8)/Emu/PICO/launch.sh
+++ static/packages/Emu/PICO-8 (Fake8)/Emu/PICO/launch.sh
@@ -2,12 +2,9 @@
 echo $0 $*
 progdir=`dirname "$0"`
 homedir=`dirname "$1"`
+
 cd $progdir
 ./cpufreq.sh
-
-# Timer initialisation
-cd /mnt/SDCARD/App/PlayActivity
-./playActivity "init"
 
 killall audioserver
 killall audioserver.mod
@@ -16,6 +13,3 @@ HOME=$homedir $progdir/FAKE08 "$1"
 
 /mnt/SDCARD/miyoo/app/audioserver &
 
-# Timer registration
-cd /mnt/SDCARD/App/PlayActivity
-./playActivity "$1"
