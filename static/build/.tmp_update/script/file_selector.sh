932c9d199d33fef326dbf5a3e54c4fca306b2396
Aemiii91 <44569252+Aemiii91@users.noreply.github.com>
2022-12-08 21:08:50 +0100

    Fix shellect title

--- static/build/.tmp_update/script/file_selector.sh
+++ static/build/.tmp_update/script/file_selector.sh
@@ -8,7 +8,7 @@ exit=0
 while [ $exit -eq 0 ]; do
 
 	file_list=`ls -a "$current_dir" | awk '!/^ / && !/^-/ && !/==/ && (/^..$/ || !/^\./)'`
-	selected_file=`echo "$file_list" | /mnt/SDCARD/.tmp_update/script/shellect.sh -t "Video Selector" -b "Press A to play, START to exit."`
+	selected_file=`echo "$file_list" | /mnt/SDCARD/.tmp_update/script/shellect.sh -t "File Selector" -b "Press A to play, START to exit."`
 
 	if [ -d "$current_dir/$selected_file" ]; then
 		current_dir="$current_dir/$selected_file"
