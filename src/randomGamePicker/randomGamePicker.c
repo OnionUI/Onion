1ce1b54f763afa951d9691957c8139ae6477adea
Matheus Zorzete Marchiore <marchiore.matheus@gmail.com>
2022-12-12 21:23:39 -0300

    Including new Game Menu option "Random Game" (#631)
    * Adding possibility to choose emu to random roms
    
    * Including game menu action to trigger Random Game Picker app
    
    * Creating a method to read config file and removing some logs

--- src/randomGamePicker/randomGamePicker.c
+++ src/randomGamePicker/randomGamePicker.c
@@ -44,12 +44,9 @@ bool readDatabase(const char *cache_path, const char *table_name) {
     {   
         cJSON *games = cJSON_CreateObject();
         char *out;
-        printf("sqlite3_column_text(res, 3): %s\n", sqlite3_column_text(res, 2));
 
         cJSON_AddItemToObject(games, "rompath", cJSON_CreateString(sqlite3_column_text(res, 2)));
         out = cJSON_Print(games);
-        
-        printf("%s\n", out);
 
         JsonGameEntry jsonGameEntry = JsonGameEntry_fromJson(out);
 
@@ -63,42 +60,48 @@ bool readDatabase(const char *cache_path, const char *table_name) {
     return true;
 }
 
+void readConfigFile(const char *emuname) {
+
+    char config_path[512];    
+    sprintf(config_path, "/mnt/SDCARD/Emu/%s/config.json", emuname);
+
+    if (exists(config_path)) {
+        const char *config_str = file_read(config_path);
+        
+        JsonGameEntry config = JsonGameEntry_fromJson(config_str);
+        
+        char cache_path[STR_MAX * 3];
+        snprintf(cache_path, STR_MAX * 3 - 1, "%s/%s/%s/%s_cache2.db", "/mnt/SDCARD/Emu", emuname, config.rompath, basename(config.rompath));
+
+        char table_name[STR_MAX];
+        char *last = strrchr(config.rompath, '/');
+        if (last != NULL) {                    
+            snprintf(table_name, STR_MAX, "%s_roms", last+1);
+            printf_debug("table_name: '%s'\n", table_name);
+        }       
+        
+        printf_debug("cache_path: %s\n", cache_path);
+
+        readDatabase(cache_path, table_name);
+    }
+}
+
 int main(int argc, char *argv[])
 {
     DIR *dp;
     struct dirent *ep;
-    char config_path[512];
     
     if ((dp = opendir("/mnt/SDCARD/Emu")) != NULL) {
-        
-        while ((ep = readdir(dp))) {
-            if (ep->d_type != DT_DIR) continue;
 
-            sprintf(config_path, "/mnt/SDCARD/Emu/%s/config.json", ep->d_name);
-            
-            if (exists(config_path)) {
-                const char *config_str = file_read(config_path);
-                printf("config_str: %s\n", config_str);
-                
-                JsonGameEntry config = JsonGameEntry_fromJson(config_str);
-                printf("rompath: %s\n", config.rompath);
-                
-                char cache_path[STR_MAX * 3];
-                snprintf(cache_path, STR_MAX * 3 - 1, "%s/%s/%s/%s_cache2.db", "/mnt/SDCARD/Emu", ep->d_name, config.rompath, basename(config.rompath));
-
-                char table_name[STR_MAX];
-
-                printf("cache_path: %s\n", cache_path);
-                char *last = strrchr(config.rompath, '/');
-                if (last != NULL) {                    
-                    snprintf(table_name, STR_MAX, "%s_roms", last+1);
-                    printf("table_name: '%s'\n", table_name);
-                }       
-                
-                printf("cache_path: %s\n", cache_path);
-
-                readDatabase(cache_path, table_name);
-
+        if (argc > 1) {
+            // read database from a specific emu
+            readConfigFile(argv[1]);
+        } 
+        else {
+            // read database from all cache dbs
+            while ((ep = readdir(dp))) {
+                if (ep->d_type != DT_DIR) continue;
+                readConfigFile(ep->d_name);
             }
         }
         closedir(dp);
@@ -113,18 +116,16 @@ int main(int argc, char *argv[])
     srand(time(NULL));
     random_number = rand() % total_games;
 
-    printf("random_number: %d\n", random_number);
-
     char emu_path[STR_MAX];
     concat(emu_path, all_games[random_number].emupath, "/launch.sh");
 
     char cmd_to_run[STR_MAX];
-    snprintf(cmd_to_run, STR_MAX, "%s \"%s\" \"%s\"", "LD_PRELOAD=/mnt/SDCARD/miyoo/app/../lib/libpadsp.so", emu_path, all_games[random_number].rompath);
+    snprintf(cmd_to_run, STR_MAX * 3, "%s \"%s\" \"%s\"", "LD_PRELOAD=/mnt/SDCARD/miyoo/app/../lib/libpadsp.so", emu_path, all_games[random_number].rompath);
     
     FILE *fp;
     file_put_sync(fp, "/mnt/SDCARD/.tmp_update/cmd_to_run.sh", "%s", cmd_to_run);
 
-    printf("cmd_to_run: %s\n", cmd_to_run);
+    printf_debug("cmd_to_run: %s\n", cmd_to_run);
 
     temp_flag_set("quick_switch", true);
 
