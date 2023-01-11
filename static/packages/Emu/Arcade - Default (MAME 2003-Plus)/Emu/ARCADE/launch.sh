1864dc1fd9c0e94242fc9b24b5d78121af806ca8
Schmurtz <Schmurtz@gmail.com>
2023-01-11 01:07:01 +0100

    Add a video tutorial for arcade save states management
    This video is played only once the first time you run an arcade game with Mame2003Plus. After that you can find it in the "video player" application of your Miyoo Mini.
    
    Script :
    
    "On Onion, the use of the Arcade Emulator is special:
    
    To exit a game, press menu and select at the same time.
    At the root of the menu, select "quit retroarch".
    Save game state is automatic when you quit your game.
    
    But the restoration of the save states is manual.
    Once your game is running, Press menu + select to go in retroarch menu.
    Then select save states option.
    Select "State Slot" -1
    
    It's all done, you can pick up the game where you left off
    This video is played only once, you can find it in the "video player" application of your Miyoo Mini."

--- static/packages/Emu/Arcade - Default (MAME 2003-Plus)/Emu/ARCADE/launch.sh
+++ static/packages/Emu/Arcade - Default (MAME 2003-Plus)/Emu/ARCADE/launch.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+sysdir=/mnt/SDCARD/.tmp_update
+savedir=/mnt/SDCARD/Saves/CurrentProfile/saves
+
 echo $0 $*
 progdir=`dirname "$0"`
 homedir=`dirname "$1"`
@@ -6,6 +10,34 @@ homedir=`dirname "$1"`
 # Timer initialisation
 cd /mnt/SDCARD/App/PlayActivity
 ./playActivity "init"
+
+# Arcade save state tutorial (played on the first launch only)
+
+if ! [ -f "$savedir/MAME 2003-Plus/.mame2003plusTutorialDone" ]; then 
+	
+	mkdir "$savedir/MAME 2003-Plus"
+	
+	LD_PRELOAD=/mnt/SDCARD/miyoo/lib/libpadsp.so /mnt/SDCARD/.tmp_update/bin/prompt -r \
+	-t "Arcade Save States Tutorial" \
+	-m "Watch a quick video to learn\nhow to use arcade save tates ?" \
+	"Yes" \
+	"No"
+
+	retcode=$?
+
+	if [ $retcode -eq 0 ]; then
+			cd $sysdir
+			touch /tmp/stay_awake
+			./bin/ffplay -autoexit -vf "hflip,vflip" -i "/mnt/SDCARD/Media/Videos/Onion/Mame2003Plus Save State Tutorial.mp4"
+			rm -f /tmp/stay_awake
+	fi
+
+	touch "$savedir/MAME 2003-Plus/.mame2003plusTutorialDone"
+fi
+
+
+
+
 
 cd /mnt/SDCARD/RetroArch/
 HOME=/mnt/SDCARD/RetroArch/ $progdir/../../RetroArch/retroarch -v -L $progdir/../../RetroArch/.retroarch/cores/mame2003_plus_libretro.so "$1"
