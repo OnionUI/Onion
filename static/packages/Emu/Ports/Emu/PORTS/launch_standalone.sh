be04876be8c5a67905a4da6595d81fab25d338be
Schmurtz <Schmurtz@gmail.com>
2023-03-08 01:23:28 +0100

    Adding  Ports shortcuts
    Adding new Ports launch scripts
    
    ** Which files are present : **
    - only config files and engines built for non-free games.
    - no proprietary files
    - no free games files
    - cover are included
    
    **Why ?**
     - Free games will be offered as packs (all required files will be included in each pack so no need to keep free game files in Onion)
    - built engines are hard to find (sometimes on StupidHoroscope repo, sometime Steward-Fu repo , sometimes on mine , sometime on Eggs dropbox...) so it's better to offer it directly to ours users: only licensed data files from the original game will be required from the user.
    - pre-configured config files are included : it allows to users to have the best settings and an unform key mapping for FPS (credit Axcelon)
    - games covers are included as it's not a part of original game assets, so it will help our users to have it directly.
    
    By default, only `~Import ports` shorctut will be visible in the rom list. Running it will reveal currently installed ports (and remove non installed port shortcuts).
    Even without importing anything 2048 and MrBoom will appear because these are free games which require only RA cores .

---
+++ static/packages/Emu/Ports/Emu/PORTS/launch_standalone.sh
@@ -0,0 +1,90 @@
+#!/bin/sh
+
+echo -ne "\n\n"
+echo --------------------------------------------------------------------
+echo ":: STANDALONE PORT LAUNCH"
+echo --------------------------------------------------------------------
+
+GameName="$1"
+GameDir="/mnt/SDCARD/Roms/PORTS/Games/$2"
+GameExecutable="$3"
+Arguments="$4"
+GameDataFile="$5"
+KillAudioserver="$6"
+PerformanceMode="$7"
+
+
+if [ -z "$GameDataFile" ]; then
+	echo " No GameDataFile specified, taking executable instead for presence checker."
+	GameDataFile="$GameExecutable"
+fi
+
+echo GameName ---------- : $GameName
+echo GameDir ----------- : $GameDir
+echo GameExecutable ---- : $GameExecutable
+echo Arguments --------- : $Arguments
+echo GameDataFile ------ : $GameDataFile
+echo KillAudioserver --- : $KillAudioserver
+echo PerformanceMode --- : $PerformanceMode
+
+echo find "$GameDir" -maxdepth 1 -type f -iname "$GameDataFile" | grep .
+find "$GameDir" -maxdepth 1 -type f -iname "$GameDataFile" | grep .
+if [ ! $? -eq 0 ]; then
+	echo "$GameDataFile is missing in\nRoms/PORTS/Games/$2.">/tmp/MissingPortFile.tmp
+	exit 66
+fi
+
+
+echo --------------------------------------------------------------------
+echo ":: APPLYING ADDITIONNAL CONFIGURATION"
+echo --------------------------------------------------------------------
+
+if [ "$KillAudioserver" = "1" ]; then pkill -9 -f audioserver; fi
+if [ "$PerformanceMode" = "1" ]; then echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor; fi
+
+ParasytePath="/mnt/SDCARD/.tmp_update/lib/parasyte"
+
+if [ -d "$GameDir/lib" ]; then
+	echo "exporting \"$GameDir/lib\" to LD_LIBRARY_PATH"
+	export LD_LIBRARY_PATH="$GameDir/lib:$ParasytePath:$LD_LIBRARY_PATH:$GameDir/lib"
+fi
+
+
+if [ -d "$GameDir/libs" ]; then
+	echo "exporting \"$GameDir/libs\" to LD_LIBRARY_PATH"
+	export LD_LIBRARY_PATH="$GameDir/libs:$ParasytePath:$LD_LIBRARY_PATH"
+fi
+
+
+echo --------------------------------------------------------------------
+echo ":: RUNNING THE PORT"
+echo --------------------------------------------------------------------
+
+cd "$GameDir"
+HOME="$GameDir"
+
+echo running "$GameName" ...
+
+
+eval echo -ne "Command line : \\\n\"$GameDir/$GameExecutable\" $Arguments \\\n\\\n\\\n"
+eval  \"$GameDir/$GameExecutable\" $Arguments
+
+
+echo --------------------------------------------------------------------
+echo ":: POST RUNNING TASKS"
+echo --------------------------------------------------------------------
+
+unset LD_PRELOAD
+if [ "$KillAudioserver" = "1" ] 
+	then /mnt/SDCARD/miyoo/app/audioserver &
+fi
+
+echo -ne "\n\n" 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
