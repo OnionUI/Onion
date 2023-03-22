b4644791c29d86a333b189d97a1313a1584a9a55
Aemiii91 <44569252+Aemiii91@users.noreply.github.com>
2023-03-22 20:32:06 +0100

    Show message in Gallery app when no screenshots present

--- static/packages/App/Gallery (Screenshot viewer)/App/Screenshots_Viewer/launch.sh
+++ static/packages/App/Gallery (Screenshot viewer)/App/Screenshots_Viewer/launch.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
-cd /mnt/SDCARD/App/Screenshots_Viewer
+echo ":: Gallery - Screenshots Viewer ::"
+
 /mnt/SDCARD/.tmp_update/bin/infoPanel -d /mnt/SDCARD/Screenshots --show-theme-controls
+ec=$?
+
+if [ $ec -ne 0 ]; then
+    /mnt/SDCARD/.tmp_update/bin/infoPanel -t Gallery -m "No screenshots found"
+fi
