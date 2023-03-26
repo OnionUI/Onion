2e683ea5bdae8b58b09701689078c3efa9ea5e57
Aemiii91 <44569252+Aemiii91@users.noreply.github.com>
2023-03-26 18:48:29 +0200

    Switched `advancemame` over to using `miyoocmd`

--- static/packages/RApp/Arcade (AdvanceMame)/RApp/advancemame/launch.sh
+++ static/packages/RApp/Arcade (AdvanceMame)/RApp/advancemame/launch.sh
@@ -2,49 +2,20 @@
 echo $0 $*
 sysdir=/mnt/SDCARD/.tmp_update
 progdir=`dirname "$0"`
-homedir=`dirname "$1"`
-romname=`basename $1`
 rompath="$1"
-filename=`basename "$rompath"`
+romname=`basename "$rompath"`
+ext=`echo "$(basename "$rompath")" | awk -F. '{print tolower($NF)}'`
 
 cd $progdir
 
-
-if [ "$filename" = "~Run advmenu.shortcut" ]; then
-	echo "Running advancemenu now !"
-	
-	while : ; do
-		HOME=$progdir ./advmenu
-		# Free memory
-        $sysdir/bin/freemma
-	
-		if [ -f /tmp/advmameRunning ] ; then
-			# Set CPU performance mode
-			./cpufreq.sh
-			# Run AdvanceMame
-			value=`cat /tmp/advmameRunning`
-			HOME=$progdir ./advmame "$value"
-			# Free memory
-			$sysdir/bin/freemma
-			rm /tmp/advmameRunning
-			echo ondemand > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-		else
-			break
-		fi
-	done
-
-	
-else
-	echo "Running game : \"$rompath\""
-	# set CPU performance mode
-	./cpufreq.sh
-	# Running advancemame
-	HOME=$progdir ./advmame "${romname%.*}" 
-	
+if [ "$ext" = "miyoocmd" ]; then
+	chmod a+x "$rompath"
+	"$rompath"
+	exit
 fi
 
-
-
-
-
-
+echo "Running game : \"$rompath\""
+# set CPU performance mode
+./cpufreq.sh
+# Running advancemame
+HOME=$progdir ./advmame "${romname%.*}" 
