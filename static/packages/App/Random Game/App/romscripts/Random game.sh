9b0f7ae2c4060da511094e7e37a65c9b95c11668
Aemiii91 <44569252+Aemiii91@users.noreply.github.com>
2023-03-27 02:13:19 +0200

    Improved `randomGamePicker` * Is now truly random (all games have equal chance) * Random game script now works with recents/favorites * Random game is now prompted to the user (A to launch, B to cancel) * Random game prompt: Press Y to toggle between box art / game info

--- static/packages/App/Random Game/App/romscripts/Random game.sh
+++ static/packages/App/Random Game/App/romscripts/Random game.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
-sysdir=/mnt/SDCARD/.tmp_update
-emupath=`echo "$1" | awk '{st = index($0,"/../../"); print substr($0,0,st-1)}'`
-emuname=`basename "$emupath"`
+scriptlabel="Random game (%LIST%)"
 
-cd $sysdir
-./bin/randomGamePicker "$emuname"
+TAB_GAMES=1
+TAB_FAVORITES=2
+TAB_APPS=3
+TAB_RECENTS=10
+TAB_EXPERT=16
+current_tab=$(cat /tmp/state.json | grep "\"type\":" | sed -e 's/^.*:\s*//g' | sed -e 's/\s*,$//g' | xargs | awk '{ print $2 }')
 
-exit 0
+if [ $current_tab -eq $TAB_GAMES ] || [ $current_tab -eq $TAB_EXPERT ]; then
+    arg=`pwd -P`
+elif [ $current_tab -eq $TAB_RECENTS ]; then
+    arg="--recents"
+elif [ $current_tab -eq $TAB_FAVORITES ]; then
+    arg="--favorites"
+else
+    exit
+fi
+
+cd /mnt/SDCARD/App/RandomGamePicker
+
+./random.sh "$arg"
