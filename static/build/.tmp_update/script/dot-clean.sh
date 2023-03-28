257deb88970725ce2f9c267f963962f34fb082c7
Aemiii91 <44569252+Aemiii91@users.noreply.github.com>
2023-03-29 01:08:13 +0200

    `dot-clean`: exclude video "seen" files

--- static/build/.tmp_update/script/dot-clean.sh
+++ static/build/.tmp_update/script/dot-clean.sh
@@ -4,6 +4,6 @@ cd /mnt/SDCARD/
 
 rm -rf .Spotlight-V100 .apDisk .fseventsd .TemporaryItems .Trash .Trashes
 
-find ./Roms ./Media -depth -type f \( -name "._*" -o -name ".DS_Store" \) -delete;
+find ./Roms ./Media -depth -type f \( -name "._*" -o -name ".DS_Store" \) -not -path "**/._state_seen/*" -delete
 find ./Roms ./Media -depth -type d -name "__MACOSX" -exec rm -rf {} \;
 find ./Roms -type f -name "*_cache2.db" -exec rm -f {} \;
