7f9b3e038a0633a7541d73b87d4bc11b3d3e2be0
Ygor Dreyer <dreyyer@gmail.com>
2023-04-03 16:20:57 -0300

    Kill wifi when killing MainUI

--- src/common/system/system_utils.h
+++ src/common/system/system_utils.h
@@ -17,12 +17,22 @@ int check_autosave(void)
     return strcmp(value, "true") == 0;
 }
 
+void kill_wifi(void)
+{
+    FILE *fp;
+    char cmd[STR_MAX * 4];
+    snprintf(cmd, STR_MAX * 4 - 1,
+             "pkill -9 wpa_supplicant; pkill -9 udhcpc; pkill -9 sshd;");
+    file_put_sync(fp, "/tmp/cmd_to_run.sh", "%s", cmd);
+}
+
 void kill_mainUI(void)
 {
     if (system_state == MODE_MAIN_UI) {
         settings_shm_read();
         temp_flag_set("mainui_killed", true);
         sync();
+        kill_wifi();
         kill(system_state_pid, SIGKILL);
         display_reset();
     }
