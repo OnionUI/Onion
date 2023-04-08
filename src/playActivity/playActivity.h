f7b1ad557abf2f1ce4e140c1a6c6ae80b783c680
nil <nilobject@gmail.com>
2023-04-09 05:33:29 +1000

    header

--- src/playActivity/playActivity.h
+++ src/playActivity/playActivity.h
@@ -1,3 +1,6 @@
+#ifndef PLAY_ACTIVITY_H
+#define PLAY_ACTIVITY_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,3 +14,7 @@
 #define INIT_TIMER_PATH "/tmp/initTimer"
 #define PLAY_ACTIVITY_SQLITE_PATH "/mnt/SDCARD/Saves/CurrentProfile/saves/play_activity.sqlite"
 #define PLAY_ACTIVITY_DB_PATH "/mnt/SDCARD/Saves/CurrentProfile/saves/playActivity.db"
+
+int get_play_time(const char* name);
+
+#endif
