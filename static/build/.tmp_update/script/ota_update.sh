854e2a96d4a47f3fef9b7af93f0f2ef6988f3d1e
r00t8ug83 <weiyann83@gmail.com>
2023-04-10 17:23:15 +0800

    Update ota_update.sh
    more effective way for connection checking

--- static/build/.tmp_update/script/ota_update.sh
+++ static/build/.tmp_update/script/ota_update.sh
@@ -42,7 +42,8 @@ else
 fi
 
 echo checking internet connection...
-if ping -4 -c 4 github.com &> /dev/null
+echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
+if [ $? -eq 0 ];
 then
     echo "Internet check successful"
 else
