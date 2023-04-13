c437b9117b686ab100c642e74dc8e585a944e478
nil <nilobject@gmail.com>
2023-04-13 23:34:25 +1000

    log

--- src/playActivity/playActivity.h
+++ src/playActivity/playActivity.h
@@ -35,10 +35,10 @@ void close_db(void) {
 PlayActivity ** find_play_activities(const char *name) {
     printf_debug("find(%s)\n", name);
     PlayActivity **play_activities = NULL;
-    char* sql = sqlite3_mprintf("SELECT * FROM play_activities WHERE name LIKE CONCAT('%|','%q','|%';", name);
-    sqlite3_stmt* stmt;
+    char *sql = sqlite3_mprintf("SELECT * FROM play_activities WHERE name LIKE CONCAT('%|','%q','|%';", name);
+    sqlite3_stmt *stmt;
     int rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
-    printf_debug("%s", sqlite3_expanded_sql(stmt));
+    printf_debug("%s", sqlite3_normalized_sql(stmt));
     if (rc != SQLITE_OK) {
         printf_debug("Error preparing SQL statement: %s\n", sqlite3_errmsg(db));
     } else {
