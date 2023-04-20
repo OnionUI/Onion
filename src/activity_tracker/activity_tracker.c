b8125c4ffc80f86b334502f6548a1941816baedb
nil <nilobject@gmail.com>
2023-04-21 04:31:34 +1000

    redesign

--- src/activity_tracker/activity_tracker.c
+++ src/activity_tracker/activity_tracker.c
@@ -73,7 +73,7 @@ int main(int argc, char *argv[])
             SDL_SoftStretch(loadedRomImage, &src_rect, imageRomImage, &dst_rect);
             imageRomPlayTime = TTF_RenderUTF8_Blended(font40, play_time_total_formatted, color_white);
             imageRomName = TTF_RenderUTF8_Blended(fontRomName25, play_activities->play_activity[index]->rom->name, color_white);
-            snprintf(details, 100, "count: %d last: %s, average: %d:%02d", play_activities->play_activity[index]->play_count, play_activities->play_activity[index]->last_played_at, play_activities->play_activity[index]->play_time_average/3600, (play_activities->play_activity[index]->play_time_average-((play_activities->play_activity[index]->play_time_average/3600)*3600))/60);
+            snprintf(details, 100, "count: %d average: %d:%02d", play_activities->play_activity[index]->play_count, play_activities->play_activity[index]->play_time_average/3600, (play_activities->play_activity[index]->play_time_average-((play_activities->play_activity[index]->play_time_average/3600)*3600))/60);
             imageRomDetails = TTF_RenderUTF8_Blended(fontRomName25, details, color_white);
             SDL_Rect rectPosition = {16, 78 + 90 * i, 50, 39};
             SDL_Rect rectRomImage = {70, 70 + 90 * i, 80, 80};
@@ -144,7 +144,7 @@ int main(int argc, char *argv[])
                 SDL_SoftStretch(loadedRomImage, &src_rect, imageRomImage, &dst_rect);
                 imageRomPlayTime = TTF_RenderUTF8_Blended(font40, play_time_total_formatted, color_white);
                 imageRomName = TTF_RenderUTF8_Blended(fontRomName25, play_activities->play_activity[index]->rom->name, color_white);
-                snprintf(details, 100, "count: %d last: %s, average: %d:%02d", play_activities->play_activity[index]->play_count, play_activities->play_activity[index]->last_played_at, play_activities->play_activity[index]->play_time_average/3600, (play_activities->play_activity[index]->play_time_average-((play_activities->play_activity[index]->play_time_average/3600)*3600))/60);
+                snprintf(details, 100, "count: %d average: %d:%02d", play_activities->play_activity[index]->play_count, play_activities->play_activity[index]->play_time_average/3600, (play_activities->play_activity[index]->play_time_average-((play_activities->play_activity[index]->play_time_average/3600)*3600))/60);
                 imageRomDetails = TTF_RenderUTF8_Blended(fontRomName25, details, color_white);
                 SDL_Rect rectPosition = {16, 78 + 90 * i, 50, 39};
                 SDL_Rect rectRomImage = {70, 70 + 90 * i, 80, 80};
