c1d0dd06fe8f9cbff706b1e25e12884662ce6086
nil <nilobject@gmail.com>
2023-04-21 04:52:08 +1000

    redesign

--- src/activity_tracker/activity_tracker.c
+++ src/activity_tracker/activity_tracker.c
@@ -79,7 +79,7 @@ int main(int argc, char *argv[])
             SDL_Rect rectRomImage = {70, 70 + 90 * i, 80, 80};
             SDL_Rect rectRomPlayTime = {170, 60 + 90 * i, 100, 56};
             SDL_Rect rectRomNames = {170, 100 + 90 * i, 400, 40};
-            SDL_Rect rectRomDetails = {170, 130 + 90 * i, 400, 40};
+            SDL_Rect rectRomDetails = {170, 120 + 90 * i, 400, 40};
             SDL_BlitSurface(imageRomPosition, NULL, screen, &rectPosition);
             SDL_BlitSurface(imageRomImage, NULL, screen, &rectRomImage);
             SDL_BlitSurface(imageRomPlayTime, NULL, screen, &rectRomPlayTime);
@@ -150,7 +150,7 @@ int main(int argc, char *argv[])
                 SDL_Rect rectRomImage = {70, 70 + 90 * i, 80, 80};
                 SDL_Rect rectRomPlayTime = {170, 60 + 90 * i, 100, 56};
                 SDL_Rect rectRomNames = {170, 100 + 90 * i, 400, 40};
-                SDL_Rect rectRomDetails = {170, 130 + 90 * i, 400, 40};
+                SDL_Rect rectRomDetails = {170, 120 + 90 * i, 400, 40};
                 SDL_BlitSurface(imageRomPosition, NULL, screen, &rectPosition);
                 SDL_BlitSurface(imageRomImage, NULL, screen, &rectRomImage);
                 SDL_BlitSurface(imageRomPlayTime, NULL, screen, &rectRomPlayTime);
