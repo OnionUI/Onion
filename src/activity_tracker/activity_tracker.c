ddb5fd05508a19ae85039d047e88d7909cfeb51f
nil <nilobject@gmail.com>
2023-04-21 04:20:11 +1000

    redesign

--- src/activity_tracker/activity_tracker.c
+++ src/activity_tracker/activity_tracker.c
@@ -150,7 +150,7 @@ int main(int argc, char *argv[])
                 SDL_Rect rectRomImage = {70, 70 + 90 * i, 80, 80};
                 SDL_Rect rectRomPlayTime = {170, 66 + 90 * i, 100, 56};
                 SDL_Rect rectRomNames = {170, 110 + 90 * i, 400, 40};
-                SDL_Rect rectRomDetails = {170, 155 + 90 * i, 400, 40};
+                SDL_Rect rectRomDetails = {170, 165 + 90 * i, 400, 40};
                 SDL_BlitSurface(imageRomPosition, NULL, screen, &rectPosition);
                 SDL_BlitSurface(imageRomImage, NULL, screen, &rectRomImage);
                 SDL_BlitSurface(imageRomPlayTime, NULL, screen, &rectRomPlayTime);
