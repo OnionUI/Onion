85ebd2a08e2b9c2b8dab424481d1c3608b914cb1
anzz1 <anzz1@live.com>
2023-04-24 05:37:46 +0300

    replace gesftpserver with sftp-server from openssh
    its simpler, stabler and uses less memory

--- static/packages/App/SSH (dropbear)/App/Ssh/launch.sh
+++ static/packages/App/SSH (dropbear)/App/Ssh/launch.sh
@@ -4,7 +4,7 @@ LD_PRELOAD=/mnt/SDCARD/miyoo/lib/libpadsp.so /mnt/SDCARD/.tmp_update/bin/infoPan
 
 cd $(dirname "$0")
 
-pkill -9 gesftpserver
+pkill -9 sftp-server
 pkill -9 dropbear
 
 /mnt/SDCARD/App/Ssh/dropbear -R -B -z > /dev/null 2>&1 &
--- static/packages/App/SSH (dropbear)/App/Ssh/uninstall.sh
+++ static/packages/App/SSH (dropbear)/App/Ssh/uninstall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-pkill -9 gesftpserver
+pkill -9 sftp-server
 pkill -9 dropbear
 rm -f /appconfigs/dropbear_dss_host_key
 rm -f /appconfigs/dropbear_rsa_host_key
