32850bcf629da7f55430b97e3a44fc5fda9497c3
XK9274 <XK9274@users.noreply.github.com>
2023-06-05 17:41:42 +0000

    🎨 apply clang-format changes

--- src/tweaks/actions.h
+++ src/tweaks/actions.h
@@ -107,17 +107,17 @@ void action_settzselectstate(void *pt)
     settings.tzselect_state = ((ListItem *)pt)->value;
 }
 
-void action_settelnetauthstate(void* pt)
+void action_settelnetauthstate(void *pt)
 {
-    settings.auth_telnet_state = ((ListItem*)pt)->value == 1;
+    settings.auth_telnet_state = ((ListItem *)pt)->value == 1;
 }
 
-void action_setftpauthstate(void* pt)
+void action_setftpauthstate(void *pt)
 {
-    settings.auth_ftp_state = ((ListItem*)pt)->value == 1;
+    settings.auth_ftp_state = ((ListItem *)pt)->value == 1;
 }
 
-void action_wpsconnection(void* pt)
+void action_wpsconnection(void *pt)
 {
     system("sh /mnt/SDCARD/.tmp_update/script/wpsclient.sh");
 }
