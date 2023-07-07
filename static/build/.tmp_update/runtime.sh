0d39c62a4fb5804b3f31dae32fd21e8e2afa8775
tGecko <tGecko@proton.me>
2023-07-07 21:35:36 +0200

    FIX: MM showing MY354 instead of MY283 (#989)
    Currently, the MainUI binaries for the 283 show MY354 in the
    Settings/About device screen.
    Fixed by patching the 283-clean/283-expert binaries.
    
    Additionaly, mount MainUI instead of copying it. This should reduce wear
    of the SD card.
    It might also fix the bug where the wrong MainUI would be loaded ("No
    Wifi option on MM+")
    since there is a lot less of file checking going on.
    
    ![MainUI_000](https://github.com/OnionUI/Onion/assets/6023076/577e24db-7502-4a1e-b83b-d8604b425dcd)
    
    ---------
    
    Co-authored-by: Aemiii91 <44569252+Aemiii91@users.noreply.github.com>

--- static/build/.tmp_update/runtime.sh
+++ static/build/.tmp_update/runtime.sh
@@ -4,8 +4,15 @@ miyoodir=/mnt/SDCARD/miyoo
 export LD_LIBRARY_PATH="/lib:/config/lib:$miyoodir/lib:$sysdir/lib:$sysdir/lib/parasyte"
 export PATH="$sysdir/bin:$PATH"
 
+MODEL_MM=283
+MODEL_MMP=354
+
 main() {
-    check_device_model
+    # Set model ID
+    axp 0 > /dev/null
+    export DEVICE_ID=$([ $? -eq 0 ] && echo $MODEL_MMP || echo $MODEL_MM)
+    echo -n "$DEVICE_ID" > /tmp/deviceModel
+
     init_system
     update_time
     clear_logs
@@ -20,22 +27,20 @@ main() {
     if [ "$system_theme" == "./" ] || [ "$system_theme" != "$active_theme" ] || [ ! -d "$system_theme" ]; then
         themeSwitcher --reapply_icons
     fi
-    
-    if [ $deviceModel -eq 283 ]; then 
-        if [ `cat /sys/devices/gpiochip0/gpio/gpio59/value` -eq 1 ]; then
-            cd $sysdir
-            chargingState
-        fi
-    elif [ $deviceModel -eq 354 ]; then 
-        cd /customer/app/
-        batteryStatus=`./axp_test`
-        case $batteryStatus in
-        *\"charging\":3* ) 
-            cd $sysdir
-            chargingState
-        esac
+
+    # Check is charging
+    if [ $DEVICE_ID -eq $MODEL_MM ]; then
+        is_charging=$(cat /sys/devices/gpiochip0/gpio/gpio59/value)
+    elif [ $DEVICE_ID -eq $MODEL_MMP ]; then
+        is_charging=$(axp 0 | grep -q "value:c4" && echo 1 || echo 0)
     fi
-    
+
+    # Show charging animation
+    if [ $is_charging -eq 1 ]; then
+        cd $sysdir
+        chargingState
+    fi
+
     # Make sure MainUI doesn't show charging animation
     touch /tmp/no_charging_ui
 
@@ -49,6 +54,7 @@ main() {
     rm /tmp/.offOrder 2> /dev/null
     HOME=/mnt/SDCARD/RetroArch/
 
+    # Detect if MENU button is held
     detectKey 1
     menu_pressed=$?
 
@@ -56,24 +62,24 @@ main() {
         rm -f "$sysdir/cmd_to_run.sh" 2> /dev/null
     fi
 
-    if [ $deviceModel -eq 354 ] && [ -f /mnt/SDCARD/RetroArch/retroarch_miyoo354 ]; then
+    if [ $DEVICE_ID -eq $MODEL_MMP ] && [ -f /mnt/SDCARD/RetroArch/retroarch_miyoo354 ]; then
         # Mount miyoo354 RA version
         mount -o bind /mnt/SDCARD/RetroArch/retroarch_miyoo354 /mnt/SDCARD/RetroArch/retroarch
     fi
 
     # Bind arcade name library to customer path
-    mount -o bind /mnt/SDCARD/miyoo/lib/libgamename.so /customer/lib/libgamename.so
-	
-	if [ -f "$sysdir/config/filebrowser/first.run" ]; then
-		# Set filebrowser branding to "Onion" and apply custom theme
-		$sysdir/bin/filebrowser config set --branding.name "Onion" -d $sysdir/config/filebrowser/filebrowser.db
-		$sysdir/bin/filebrowser config set --branding.files "$sysdir/config/filebrowser/theme" -d $sysdir/config/filebrowser/filebrowser.db
-		
-		rm "$sysdir/config/filebrowser/first.run"
-	fi
+    mount -o bind $miyoodir/lib/libgamename.so /customer/lib/libgamename.so
+
+    # Set filebrowser branding to "Onion" and apply custom theme
+    if [ -f "$sysdir/config/filebrowser/first.run" ]; then
+        $sysdir/bin/filebrowser config set --branding.name "Onion" -d $sysdir/config/filebrowser/filebrowser.db
+        $sysdir/bin/filebrowser config set --branding.files "$sysdir/config/filebrowser/theme" -d $sysdir/config/filebrowser/filebrowser.db
+
+        rm "$sysdir/config/filebrowser/first.run"
+    fi
 
     start_networking
-		
+
     # Auto launch
     if [ ! -f $sysdir/config/.noAutoStart ]; then
         state_change check_game
@@ -81,23 +87,23 @@ main() {
         rm -f "$sysdir/cmd_to_run.sh" 2> /dev/null
     fi
 
-    startup_app=`cat $sysdir/config/startup/app`
+    startup_app=$(cat $sysdir/config/startup/app)
 
     if [ $startup_app -eq 1 ]; then
-        echo -e "\n\n:: STARTUP APP: GameSwitcher\n\n"
+        log "\n\n:: STARTUP APP: GameSwitcher\n\n"
         touch $sysdir/.runGameSwitcher
     elif [ $startup_app -eq 2 ]; then
-        echo -e "\n\n:: STARTUP APP: RetroArch\n\n"
+        log "\n\n:: STARTUP APP: RetroArch\n\n"
         echo "LD_PRELOAD=$miyoodir/lib/libpadsp.so ./retroarch -v" > $sysdir/cmd_to_run.sh
         touch /tmp/quick_switch
     elif [ $startup_app -eq 3 ]; then
-        echo -e "\n\n:: STARTUP APP: AdvanceMENU\n\n"
+        log "\n\n:: STARTUP APP: AdvanceMENU\n\n"
         touch /tmp/run_advmenu
     fi
 
     state_change check_switcher
     set_startup_tab
-	
+
     # Main runtime loop
     while true; do
         state_change check_main_ui
@@ -115,9 +121,13 @@ state_change() {
     eval "$1"
 }
 
+set_prev_state() {
+    echo "$1" > /tmp/prev_state
+}
+
 clear_logs() {
     mkdir -p $sysdir/logs
-    
+
     cd $sysdir
     rm -f \
         ./logs/MainUI.log \
@@ -126,13 +136,13 @@ clear_logs() {
         ./logs/game_list_options.log \
         ./logs/network.log \
         ./logs/dnsmasq.log \
-		./logs/ftp.log \
-		./logs/ra_quick_host.log \
+        ./logs/ftp.log \
+        ./logs/ra_quick_host.log \
         2> /dev/null
 }
 
 check_main_ui() {
-    if [ ! -f $sysdir/cmd_to_run.sh ] ; then
+    if [ ! -f $sysdir/cmd_to_run.sh ]; then
         if [ -f /tmp/run_advmenu ]; then
             rm /tmp/run_advmenu
             $sysdir/bin/adv/run_advmenu.sh
@@ -145,14 +155,20 @@ check_main_ui() {
 }
 
 launch_main_ui() {
-    echo -e "\n:: Launch MainUI"
+    log "\n:: Launch MainUI"
 
     cd $sysdir
+
+    # Generate battery percentage image
     mainUiBatPerc
 
+    # Hide any new recents if applicable
     check_hide_recents
-    check_hide_expert
 
+    # Ensure we've mounted the correct MainUI binary
+    mount_main_ui
+
+    # Wifi state before
     wifi_setting=$(/customer/app/jsonval wifi)
 
     start_audioserver
@@ -160,10 +176,11 @@ launch_main_ui() {
     # MainUI launch
     cd $miyoodir/app
     PATH="$miyoodir/app:$PATH" \
-    LD_LIBRARY_PATH="$miyoodir/lib:/config/lib:/lib" \
-    LD_PRELOAD="$miyoodir/lib/libpadsp.so" \
-    ./MainUI 2>&1 > /dev/null
+        LD_LIBRARY_PATH="$miyoodir/lib:/config/lib:/lib" \
+        LD_PRELOAD="$miyoodir/lib/libpadsp.so" \
+        ./MainUI 2>&1 > /dev/null
 
+    # Check if wifi setting changed
     if [ $(/customer/app/jsonval wifi) -ne $wifi_setting ]; then
         touch /tmp/network_changed
         rm /tmp/ntp_synced 2> /dev/null
@@ -171,38 +188,34 @@ launch_main_ui() {
     fi
 
     $sysdir/bin/freemma
-    
+
     mv -f /tmp/cmd_to_run.sh $sysdir/cmd_to_run.sh
-    
-    echo "mainui" > /tmp/prev_state
+
+    set_prev_state "mainui"
 }
 
 check_game_menu() {
     if [ ! -f /tmp/launch_alt ]; then
         return
     fi
-    
+
     rm -f /tmp/launch_alt
 
     if [ ! -f $sysdir/cmd_to_run.sh ]; then
         return
     fi
-    
+
     launch_game_menu
 }
 
 launch_game_menu() {
-    echo -e "\n\n:: GLO\n\n"
+    log "\n\n:: GLO\n\n"
 
     cd $sysdir
-    if [ -f ./config/.logging ]; then
-        ./script/game_list_options.sh >> ./logs/game_list_options.log
-    else
-        ./script/game_list_options.sh
-    fi
+    ./script/game_list_options.sh
 
     if [ $? -ne 0 ]; then
-        echo -e "\n\n< Back to MainUI\n\n"
+        log "\n\n< Back to MainUI\n\n"
         rm -f $sysdir/cmd_to_run.sh 2> /dev/null
         check_off_order "End"
     fi
@@ -210,7 +223,7 @@ launch_game_menu() {
 
 check_game() {
     # Game launch
-    if  [ -f $sysdir/cmd_to_run.sh ] ; then
+    if [ -f $sysdir/cmd_to_run.sh ]; then
         launch_game
     fi
 }
@@ -220,8 +233,8 @@ check_is_game() {
 }
 
 launch_game() {
-    echo -e "\n:: Launch game"
-    cmd=`cat $sysdir/cmd_to_run.sh`
+    log "\n:: Launch game"
+    cmd=$(cat $sysdir/cmd_to_run.sh)
 
     is_game=0
     rompath=""
@@ -239,23 +252,23 @@ launch_game() {
             rompath=$(echo "$rompath" | awk '{split($0,a,":"); print a[2]}')
         fi
 
-        romext=`echo "$(basename "$rompath")" | awk -F. '{print tolower($NF)}'`
+        romext=$(echo "$(basename "$rompath")" | awk -F. '{print tolower($NF)}')
         romcfgpath="$(dirname "$rompath")/.game_config/$(basename "$rompath" ".$romext").cfg"
 
         if [ "$romext" != "miyoocmd" ]; then
-            echo "rompath: $rompath (ext: $romext)"
-            echo "romcfgpath: $romcfgpath"
+            log "rompath: $rompath (ext: $romext)"
+            log "romcfgpath: $romcfgpath"
             is_game=1
         fi
     fi
 
     if [ $is_game -eq 1 ]; then
         if [ -f "$romcfgpath" ]; then
-            romcfg=`cat "$romcfgpath"`
-            retroarch_core=`get_info_value "$romcfg" core`
+            romcfg=$(cat "$romcfgpath")
+            retroarch_core=$(get_info_value "$romcfg" core)
             corepath=".retroarch/cores/$retroarch_core.so"
 
-            echo "per game core: $retroarch_core" >> $sysdir/logs/game_list_options.log
+            log "per game core: $retroarch_core" >> $sysdir/logs/game_list_options.log
 
             if [ -f "/mnt/SDCARD/RetroArch/$corepath" ]; then
                 if echo "$cmd" | grep -q "$sysdir/reset.cfg"; then
@@ -268,7 +281,7 @@ launch_game() {
 
         # Handle dollar sign
         if echo "$rompath" | grep -q "\$"; then
-            temp=`cat $sysdir/cmd_to_run.sh`
+            temp=$(cat $sysdir/cmd_to_run.sh)
             echo "$temp" | sed 's/\$/\\\$/g' > $sysdir/cmd_to_run.sh
         fi
 
@@ -278,12 +291,12 @@ launch_game() {
     # Prevent quick switch loop
     rm -f /tmp/quick_switch 2> /dev/null
 
-    echo "----- COMMAND:"
-    cat $sysdir/cmd_to_run.sh
+    log "----- COMMAND:"
+    log "$(cat $sysdir/cmd_to_run.sh)"
 
     if [ "$romext" == "miyoocmd" ]; then
         if [ -f "$rompath" ]; then
-            emupath=`dirname $(echo "$cmd" | awk '{ gsub(/"/, "", $2); st = index($2,".."); if (st) { print substr($2,0,st) } else { print $2 } }')`
+            emupath=$(dirname $(echo "$cmd" | awk '{ gsub(/"/, "", $2); st = index($2,".."); if (st) { print substr($2,0,st) } else { print $2 } }'))
             cd "$emupath"
 
             chmod a+x "$rompath"
@@ -299,7 +312,7 @@ launch_game() {
         retval=$?
     fi
 
-    echo "cmd retval: $retval"
+    log "cmd retval: $retval"
 
     if [ $retval -ge 128 ] && [ $retval -ne 143 ] && [ $retval -ne 255 ]; then
         cd $sysdir
@@ -323,11 +336,11 @@ launch_game() {
 
         cd $sysdir
         playActivity stop "$rompath"
-        
-        echo "game" > /tmp/prev_state
+
+        set_prev_state "game"
         check_off_order "End_Save"
     else
-        echo "app" > /tmp/prev_state
+        set_prev_state "app"
         check_off_order "End"
     fi
 }
@@ -337,7 +350,7 @@ get_info_value() {
 }
 
 check_switcher() {
-    if [ -f $sysdir/.runGameSwitcher ] ; then
+    if [ -f $sysdir/.runGameSwitcher ]; then
         launch_switcher
     elif [ -f /tmp/quick_switch ]; then
         # Quick switch
@@ -347,23 +360,23 @@ check_switcher() {
         rm $sysdir/cmd_to_run.sh 2> /dev/null
         sync
     fi
-    
+
     check_off_order "End"
 }
 
 launch_switcher() {
-    echo -e "\n:: Launch switcher"
+    log "\n:: Launch switcher"
     cd $sysdir
     LD_PRELOAD="$miyoodir/lib/libpadsp.so" gameSwitcher
     rm $sysdir/.runGameSwitcher
-    echo "switcher" > /tmp/prev_state
+    set_prev_state "switcher"
     sync
 }
 
 check_off_order() {
-    if  [ -f /tmp/.offOrder ] ; then
+    if [ -f /tmp/.offOrder ]; then
         bootScreen "$1" &
-        sleep 1			# Allow the bootScreen to be displayed
+        sleep 1 # Allow the bootScreen to be displayed
         shutdown
     fi
 }
@@ -381,9 +394,9 @@ check_hide_recents() {
             mv -f $recentlist_temp $recentlist_hidden
             rm -f $recentlist
         fi
-    # Hide recents off
+        # Hide recents off
     else
-        # Restore recentlist 
+        # Restore recentlist
         if [ -f $recentlist_hidden ]; then
             cat $recentlist $recentlist_hidden > $recentlist_temp
             mv -f $recentlist_temp $recentlist
@@ -394,77 +407,39 @@ check_hide_recents() {
 }
 
 mainui_target=$miyoodir/app/MainUI
-clean_flag=$miyoodir/app/.isClean
-expert_flag=$miyoodir/app/.isExpert
 
-check_hide_expert() {
-    if [ ! -f $sysdir/config/.showExpert ]; then
-        # Should be clean
-        if [ ! -f $clean_flag ] || [ -f $expert_flag ] || [ $is_device_model_changed -eq 1 ] || [ ! -f $mainui_target ]; then
-            rm -f $mainui_target 2> /dev/null
-            rm -f $expert_flag 2> /dev/null
-            cp "$sysdir/bin/MainUI-$deviceModel-clean" $mainui_target
-            touch $clean_flag
+mount_main_ui() {
+    mainui_mode=$([ -f $sysdir/config/.showExpert ] && echo "expert" || echo "clean")
+    mainui_srcname="MainUI-$DEVICE_ID-$mainui_mode"
+    mainui_mount=$(basename "$(cat /proc/self/mountinfo | grep $mainui_target | cut -d' ' -f4)")
+
+    if [ "$mainui_mount" != "$mainui_srcname" ]; then
+        if mount | grep -q "$mainui_target"; then
+            umount $mainui_target 2> /dev/null
         fi
-    else
-        # Should be expert
-        if [ ! -f $expert_flag ] || [ -f $clean_flag ] || [ $is_device_model_changed -eq 1 ] || [ ! -f $mainui_target ]; then
-            rm -f $mainui_target 2> /dev/null
-            rm -f $clean_flag 2> /dev/null
-            cp "$sysdir/bin/MainUI-$deviceModel-expert" $mainui_target
-            touch $expert_flag
+
+        if [ ! -f $mainui_target ]; then
+            touch $mainui_target
         fi
-    fi
-    sync
-}
 
-
-deviceModel=0
-last_device_model=$miyoodir/app/lastDeviceModel
-is_device_model_changed=0
-
-check_device_model() {
-    echo -e "\n:: Check device model"
-
-    if axp 0; then
-        touch /tmp/deviceModel
-        printf "354" > /tmp/deviceModel
-        deviceModel=354
-    else
-        touch /tmp/deviceModel
-        printf "283" > /tmp/deviceModel
-        deviceModel=283
-    fi
-
-    # Check if the SD is inserted in a different model
-    is_device_model_changed=0
-    if [ ! -f $last_device_model ]; then
-        cp /tmp/deviceModel $last_device_model
-        is_device_model_changed=1
-    else
-        lastDeviceModel=`cat $last_device_model`
-        if [ $lastDeviceModel -ne $deviceModel ]; then
-            is_device_model_changed=1
-            echo $deviceModel > $last_device_model
-        fi
+        mount -o bind "$sysdir/bin/$mainui_srcname" $mainui_target
     fi
 }
-
 
 init_system() {
-    echo -e "\n:: Init system"
+    log "\n:: Init system"
 
     # init_lcd
     cat /proc/ls
     sleep 0.25
 
-    if [ $deviceModel -eq 354 ] && [ -f $sysdir/config/.lcdvolt ]; then
+    if [ $DEVICE_ID -eq $MODEL_MMP ] && [ -f $sysdir/config/.lcdvolt ]; then
         $sysdir/script/lcdvolt.sh 2> /dev/null
     fi
-    
+
     start_audioserver
 
-    if [ $deviceModel -eq 283 ]; then
+    if [ $DEVICE_ID -eq $MODEL_MM ]; then
         # init charger detection
         if [ ! -f /sys/devices/gpiochip0/gpio/gpio59/direction ]; then
             echo 59 > /sys/class/gpio/export
@@ -473,17 +448,17 @@ init_system() {
 
         if [ $(/customer/app/jsonval vol) -ne 20 ] || [ $(/customer/app/jsonval mute) -ne 0 ]; then
             # Force volume and mute settings
-            cat /appconfigs/system.json \
-                | sed 's/^\s*"vol":\s*[0-9][0-9]*/\t"vol":\t20/g' \
-                | sed 's/^\s*"mute":\s*[0-9][0-9]*/\t"mute":\t0/g' \
-                > temp
+            cat /appconfigs/system.json |
+                sed 's/^\s*"vol":\s*[0-9][0-9]*/\t"vol":\t20/g' |
+                sed 's/^\s*"mute":\s*[0-9][0-9]*/\t"mute":\t0/g' \
+                    > temp
             mv -f temp /appconfigs/system.json
         fi
     fi
 
-    brightness=`/customer/app/jsonval brightness`
-    brightness_raw=`awk "BEGIN { print int(3 * exp(0.350656 * $brightness) + 0.5) }"`
-    echo "brightness: $brightness -> $brightness_raw"
+    brightness=$(/customer/app/jsonval brightness)
+    brightness_raw=$(awk "BEGIN { print int(3 * exp(0.350656 * $brightness) + 0.5) }")
+    log "brightness: $brightness -> $brightness_raw"
 
     # init backlight
     echo 0 > /sys/class/pwm/pwmchip0/export
@@ -497,12 +472,12 @@ update_time() {
     currentTime=0
     # Load current time
     if [ -f $timepath ]; then
-        currentTime=`cat $timepath`
+        currentTime=$(cat $timepath)
     fi
     #Add 4 hours to the current time
     hours=4
     if [ -f $sysdir/config/startup/addHours ]; then
-        hours=`cat $sysdir/config/startup/addHours`
+        hours=$(cat $sysdir/config/startup/addHours)
     fi
     addTime=$(($hours * 3600))
     if [ ! -f $sysdir/config/.ntpState ]; then
@@ -514,48 +489,48 @@ update_time() {
 set_startup_tab() {
     startup_tab=0
     if [ -f $sysdir/config/startup/tab ]; then
-        startup_tab=`cat $sysdir/config/startup/tab`
+        startup_tab=$(cat $sysdir/config/startup/tab)
     fi
-    
+
     cd $sysdir
     setState "$startup_tab"
 }
 
 start_audioserver() {
-    defvol=`echo $(/customer/app/jsonval vol) | awk '{ printf "%.0f\n", 48 * (log(1 + $1) / log(10)) - 60 }'`
+    defvol=$(echo $(/customer/app/jsonval vol) | awk '{ printf "%.0f\n", 48 * (log(1 + $1) / log(10)) - 60 }')
     runifnecessary "audioserver" $miyoodir/app/audioserver $defvol
 }
 
 runifnecessary() {
     cnt=0
     #a=`ps | grep $1 | grep -v grep`
-    a=`pgrep $1`
-    while [ "$a" == "" ] && [ $cnt -lt 8 ] ; do
-        echo try to run $2
+    a=$(pgrep $1)
+    while [ "$a" == "" ] && [ $cnt -lt 8 ]; do
+        log "try to run: $2"
         $2 $3 &
         sleep 0.5
-        cnt=`expr $cnt + 1`
-        a=`pgrep $1`
+        cnt=$(expr $cnt + 1)
+        a=$(pgrep $1)
     done
 }
 
 start_networking() {
-	# Loop breaker for NTP
-	touch /tmp/ntp_run_once
+    # Loop breaker for NTP
+    touch /tmp/ntp_run_once
 
-    rm $sysdir/config/.hotspotState  # dont start hotspot at boot
-    
+    rm $sysdir/config/.hotspotState # dont start hotspot at boot
+
     touch /tmp/network_changed
     sync
 }
 
 check_networking() {
-    if [ $deviceModel -ne 354 ] || [ ! -f /tmp/network_changed ]; then
+    if [ $DEVICE_ID -ne $MODEL_MMP ] || [ ! -f /tmp/network_changed ]; then
         check_timezone
         return
     fi
-	rm /tmp/network_changed
-	
+    rm /tmp/network_changed
+
     $sysdir/script/network/update_networking.sh check
 
     check_timezone
@@ -565,4 +540,16 @@ check_timezone() {
     export TZ=$(cat "$sysdir/config/.tz")
 }
 
-main
+scriptname=$(basename "$0" .sh)
+
+log() {
+    if [ -f $sysdir/config/.logging ]; then
+        echo -e "($scriptname) $(date):" $* | tee -a "$sysdir/logs/$scriptname.log"
+    fi
+}
+
+if [ -f $sysdir/config/.logging ]; then
+    main
+else
+    main 2>&1 > /dev/null
+fi
