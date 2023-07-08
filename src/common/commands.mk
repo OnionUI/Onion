2e6f538c35631548f50c3ff7db711399ebbeda54
Aemiii91 <44569252+Aemiii91@users.noreply.github.com>
2023-07-08 23:32:24 +0200

    FIX: Improved installer (#991)
    ## Changes
    - Installation for MMP no longer relies on the transfer of `.tmp_update`
    - Unless Onion is already installed and device is on 20230505 or later
    (this will run `runtime.sh` directly)
    - Moved the "installer check" into `runtime.sh`
    - Meaning that from this release and forward, only the `miyoo` folder
    needs to be transferred to update Onion
    - Fixed the notorious "black screen installation" bug - it was caused by
    wrongful `LD_LIBRARY_PATH`
    - Added core extension cache to build step (removes initial 10s load
    time for GLO -> Game core)
    - We now have a concept of **migration scripts** !!
      - These are single-run scripts across Onion updates
    - They are numbered, and the last migration ID that ran will be saved
    persistently
    - Onion defaults changed:
      - "Color Correction" for mGBA is now OFF
      - "Don't Overwrite SaveRAM" is now ON

--- src/common/commands.mk
+++ src/common/commands.mk
@@ -1,8 +1,13 @@
 TARGET_NAME = $(shell basename "$(TARGET)")
 
 ECHO = echo
+
+COLOR_BLUE = ""
+COLOR_NORMAL = ""
+
 PRINT_BUILD = "\n-- [$(TARGET_NAME)] Building $<"
 PRINT_RECIPE = "\n:: $(TARGET_NAME) -> $@"
+PRINT_DONE = "\n-> $@ [READY!]\n"
 
 COMPILE_CC = $(CC) $(CFLAGS) -std=gnu18 -c "$<" -o "$@"
 COMPILE_CXX = $(CXX) $(CXXFLAGS) -std=c++17 -c "$<" -o "$@"
@@ -27,7 +32,11 @@ COLOR_REP = sed \
 MAKE_DEV = DEBUG=1 COLORS=1 make --no-print-directory
 
 ifeq (1,$(COLORS))
+COLOR_BLUE = "\e[1;34m"
+COLOR_NORMAL = "\e[0m"
 PRINT_BUILD = "\e[1;34m-- [$(TARGET_NAME)] Building $<\e[0m"
 PRINT_RECIPE = "\e[1;34m:: $(TARGET_NAME) -> $@\e[0m"
+PRINT_DONE = "\n\e[1;32m-> $@ [READY!]\e[0m\n"
 COMPILE_CC_OUT = "\e[1;30m> $(COMPILE_CC)\e[0m$$($(COMPILE_CC) -fdiagnostics-color=always 2>&1)"
+COMPILE_CXX_OUT = "\e[1;30m> $(COMPILE_CXX)\e[0m$$($(COMPILE_CXX) -fdiagnostics-color=always 2>&1)"
 endif
