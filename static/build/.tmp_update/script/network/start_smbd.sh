993ad3ed8d0436aa772ae0b5975c80302f92643d
tGecko <tGecko@proton.me>
2023-07-11 07:49:47 +0200

    Use unset instead of exporting empty variable

--- static/build/.tmp_update/script/network/start_smbd.sh
+++ static/build/.tmp_update/script/network/start_smbd.sh
@@ -1,6 +1,6 @@
 #unset preload or samba doesn't work correctly.
 
-export LD_PRELOAD=""
+unset LD_PRELOAD
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/mnt/SDCARD/.tmp_update/lib/samba:/mnt/SDCARD/.tmp_update/lib/samba/private"
 /mnt/SDCARD/.tmp_update/bin/samba/sbin/smbd --no-process-group -D
 PASS=smbOnion
