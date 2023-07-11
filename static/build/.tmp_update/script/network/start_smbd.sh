c3ece245c0ca646eb721c0504af5a21a8ede0053
tGecko <tGecko@proton.me>
2023-07-11 07:48:38 +0200

    Renamed the linux user to "onion"

--- static/build/.tmp_update/script/network/start_smbd.sh
+++ static/build/.tmp_update/script/network/start_smbd.sh
@@ -4,4 +4,4 @@ export LD_PRELOAD=""
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/mnt/SDCARD/.tmp_update/lib/samba:/mnt/SDCARD/.tmp_update/lib/samba/private"
 /mnt/SDCARD/.tmp_update/bin/samba/sbin/smbd --no-process-group -D
 PASS=smbOnion
-echo -ne "$PASS\n$PASS\n" | /mnt/SDCARD/.tmp_update/bin/samba/sbin/smbpasswd -s -a sduser
+echo -ne "$PASS\n$PASS\n" | /mnt/SDCARD/.tmp_update/bin/samba/sbin/smbpasswd -s -a onion
