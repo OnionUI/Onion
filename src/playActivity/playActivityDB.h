ee4d9510b0399ac97e5c65ac976c4e4a1410f0be
Aemiii91 <44569252+Aemiii91@users.noreply.github.com>
2023-07-15 03:00:37 +0200

    Fix dates in `stop_all` sql

--- src/playActivity/playActivityDB.h
+++ src/playActivity/playActivityDB.h
@@ -375,7 +375,7 @@ void play_activity_stop(char *rom_file_path)
 void play_activity_stop_all(void)
 {
     print_debug("play_activity_stop_all()");
-    play_activity_db_execute("UPDATE play_activity SET play_time = (strftime('%%s', 'now')) - created_at, updated_at = (strftime('%%s', 'now')) WHERE play_time IS NULL;");
+    play_activity_db_execute("UPDATE play_activity SET play_time = (strftime('%s', 'now')) - created_at, updated_at = (strftime('%s', 'now')) WHERE play_time IS NULL;");
 }
 
 #endif // PLAY_ACTIVITY_DB_H
