9e8b32fcd20dd45e5aef66b849ec7f089e21490e
Aemiii91 <44569252+Aemiii91@users.noreply.github.com>
2023-07-19 22:38:52 +0200

    FIX: Remove unnecessary dot-clean tool

--- src/tweaks/tools_defs.h
+++ src/tweaks/tools_defs.h
@@ -3,10 +3,9 @@
 
 #include "utils/str.h"
 
-#define NUM_TOOLS 3
+#define NUM_TOOLS 2
 
 static char tools_short_names[NUM_TOOLS][STR_MAX] = {
-    "dot_clean",
     "cue_gen",
     "build_short_rom_game_list"};
 
