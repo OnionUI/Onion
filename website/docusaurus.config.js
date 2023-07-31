948632d4fc0b521a9148da930c99d5e91dfe2a16
Aemiii91 <44569252+Aemiii91@users.noreply.github.com>
2023-08-01 01:50:06 +0200

    DOCS: Correct production url

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   trailingSlash: false,
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://onionui.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
