76e2346ff7f15e8daa968c7d67782b8cae7b8ca3
t0b10-t3nm4 <tobio.tenma@dead.dreamer.net>
2023-08-07 16:28:14 -0600

    Add back in files and continue

---
+++ scrape.temp/regions/test.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+sh find_parent.sh regions.json USA
+sleep 3
+
+sh find_parent.sh regions.json fr
+sleep 3
+
+sh find_parent.sh regions.json JAP
+sleep 3
+
+sh find_parent.sh regions.json eur
+sleep 3
