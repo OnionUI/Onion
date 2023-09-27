b361594acde62a798219e62260e342bcddea4fc0
Schmurtz <Schmurtz@gmail.com>
2023-09-28 01:05:46 +0200

    GMU : loading parasyte libs first and removing duplicates

--- static/packages/App/Music Player (GMU)/App/Gmu/launch.sh
+++ static/packages/App/Music Player (GMU)/App/Gmu/launch.sh
@@ -6,7 +6,7 @@ echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 /mnt/SDCARD/.tmp_update/script/stop_audioserver.sh
 
 HOME=/mnt/SDCARD/App/Gmu
-LD_LIBRARY_PATH=./lib:$LD_LIBRARY_PATH
+LD_LIBRARY_PATH=/mnt/SDCARD/.tmp_update/lib/parasyte:./lib:$LD_LIBRARY_PATH
 export LD_PRELOAD=./lib/libSDL-1.2.so.0
 ./gmu.bin -c gmu.miyoo.conf
 unset LD_PRELOAD
