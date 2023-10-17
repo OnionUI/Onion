5cbe505291ede4329313a010d3d4baa71381797e
Schmurtz <schmurtz@gmail.com>
2023-10-17 20:28:56 +0200

    FIX : last fixes before 4.2.0 (#1177)
    * Scraper
      * Fix Launchbox scraping
      * Fix exit on Scraper
    * Update gpSP with recommanded version from gpSP author (this version
    fix save states loading from previous versions)
    * Update GMU music player
      * add missing jpeg library to display covers
      * Adding new audio samples by Douve
      * More intuitive keymap
    * Fix sound volume after killing audio server (useful for ports and
    music player app (GMU) )
    
    ---------
    
    Co-authored-by: XK <47260768+XK9274@users.noreply.github.com>

--- static/packages/App/Music Player (GMU)/App/Gmu/launch2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo $0 $*
-cd $(dirname "$0")
-./cpufreq.sh
-HOME=/mnt/SDCARD/App/Gmu
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./lib
-export LD_PRELOAD=./lib/libSDL-1.2.so.0
-SDL_NOMOUSE=1 ./gmu.bin -c gmu.miyoo.conf &> log.txt
-unset LD_PRELOAD
