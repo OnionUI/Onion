c24cad520a8f311bb3908610ada72e84e80e7e6c
Schmurtz <Schmurtz@gmail.com>
2023-10-20 11:34:57 +0200

    Integrate NDS in Package Manager
    - Drastic 1.5 from Steward-Fu
    - removed useless files
    - removed liscenced bios file
    - removed duplicated libs which are already in Onion shrunken Parasyte
    - cpuclock moved from local folder to Onion bin files
    - removed set clock speed to 1200 at exit (already managed by MainUI)
    - use only stop_audioserver.sh
    - set "frameskip_type" to 2 and "threaded_3d" to 1
    - created a dedicated Onion icon

---
+++ static/packages/Emu/Nintendo - DS (Drastic)/Emu/NDS/launch.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+mydir=`dirname "$0"`
+
+export HOME=$mydir
+export PATH=$mydir:$PATH
+export LD_LIBRARY_PATH=$mydir/libs:$LD_LIBRARY_PATH
+export SDL_VIDEODRIVER=mmiyoo
+export SDL_AUDIODRIVER=mmiyoo
+export EGL_VIDEODRIVER=mmiyoo
+
+
+. /mnt/SDCARD/.tmp_update/script/stop_audioserver.sh
+
+CUST_LOGO=1
+CUST_CPUCLOCK=1
+
+cd $mydir
+if [ "$CUST_LOGO" == "1" ]; then
+    echo "convert resources/logo/0.png to drastic_logo_0.raw"
+    echo "convert resources/logo/1.png to drastic_logo_1.raw"
+    ./png2raw
+fi
+
+sv=`cat /proc/sys/vm/swappiness`
+
+# 60 by default
+echo 10 > /proc/sys/vm/swappiness
+
+cd $mydir
+
+if [ "$CUST_CPUCLOCK" == "1" ]; then
+    echo "set customized cpuspeed"
+    cpuclock 1500
+fi
+
+./drastic "$1"
+
+echo $sv > /proc/sys/vm/swappiness
+sync
