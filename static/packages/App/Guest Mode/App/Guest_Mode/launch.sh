1a2b9d2422a7550cf74ec8c8f3506dac387b6a13
Sichroteph <Christophe.Jeannette@gmail.com>
2023-11-09 09:40:34 +0100

    The recent lists (normal + hidden) + favorites saving

--- static/packages/App/Guest Mode/App/Guest_Mode/launch.sh
+++ static/packages/App/Guest Mode/App/Guest_Mode/launch.sh
@@ -7,17 +7,25 @@ cd $progdir
 ./saveTime.sh
 sync
 
+# Favourites + RecentList are saved
+cp /mnt/SDCARD/Roms/*.json /mnt/SDCARD/Saves/CurrentProfile/lists
+rm /mnt/SDCARD/Roms/*.json
+
 if [ -d /mnt/SDCARD/Saves/GuestProfile ] ; then
 	# The main profile is the current one
 	cp ./data/configON.json ./config.json
 	mv /mnt/SDCARD/Saves/CurrentProfile /mnt/SDCARD/Saves/MainProfile
 	mv /mnt/SDCARD/Saves/GuestProfile /mnt/SDCARD/Saves/CurrentProfile
+
 else
 	# The guest profile is the current one
 	cp ./data/configOFF.json ./config.json
 	mv /mnt/SDCARD/Saves/CurrentProfile /mnt/SDCARD/Saves/GuestProfile
 	mv /mnt/SDCARD/Saves/MainProfile /mnt/SDCARD/Saves/CurrentProfile
 fi
+
+# Favourites + RecentList are restored
+cp /mnt/SDCARD/Saves/CurrentProfile/lists/*.json /mnt/SDCARD/Roms
 
 # Load current time
 ./loadTime.sh
