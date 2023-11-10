4d7516929cb812b24c2a56f91be2340ff5fc0806
Schmurtz <Schmurtz@gmail.com>
2023-11-10 17:39:10 +0100

    Add PuzzleScript core (71f0346)
    Add PuzzleScript core in RA, + adding it in Package Manager with new icon.
    
    PuzzleScript is an open-source HTML5 puzzle game engine. All games are free an open source.
    
    Game lists :
     - https://www.puzzlescript.net/Gallery/index.html
     - https://philschatz.com/puzzlescript/
    
    Repo and instructions to download games : https://github.com/nwhitehead/pzretro

---
+++ static/packages/RApp/Game engine - PuzzleScript (PuzzleScript)/RApp/PuzzleScript/launch.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo $0 $*
+progdir=`dirname "$0"`
+homedir=`dirname "$1"`
+
+
+cd /mnt/SDCARD/RetroArch/
+HOME=/mnt/SDCARD/RetroArch/ $progdir/../../RetroArch/retroarch -v -L $progdir/../../RetroArch/.retroarch/cores/puzzlescript_libretro.so "$1"
