527483339efa047ffdb3002943c9d762f72c9422
Sichroteph <Christophe.Jeannette@gmail.com>
2023-11-16 20:56:40 +0100

    drastic 1.6 update

--- static/packages/Emu/Nintendo - DS (Drastic)/Emu/NDS/launch.sh
+++ static/packages/Emu/Nintendo - DS (Drastic)/Emu/NDS/launch.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 mydir=`dirname "$0"`
 
+cd $mydir
+if [ ! -f "/tmp/.show_hotkeys" ]; then
+    touch /tmp/.show_hotkeys
+    LD_LIBRARY_PATH=./libs:/customer/lib:/config/lib ./show_hotkeys
+fi
+
 export HOME=$mydir
 export PATH=$mydir:$PATH
 export LD_LIBRARY_PATH=$mydir/libs:$LD_LIBRARY_PATH
@@ -8,16 +14,18 @@ export SDL_VIDEODRIVER=mmiyoo
 export SDL_AUDIODRIVER=mmiyoo
 export EGL_VIDEODRIVER=mmiyoo
 
+if [ -f /mnt/SDCARD/.tmp_update/script/stop_audioserver.sh ]; then
+    /mnt/SDCARD/.tmp_update/script/stop_audioserver.sh
+else
+    killall audioserver
+    killall audioserver.mod
+fi
 
-. /mnt/SDCARD/.tmp_update/script/stop_audioserver.sh
-
-CUST_LOGO=1
+CUST_LOGO=0
 CUST_CPUCLOCK=1
 
 cd $mydir
 if [ "$CUST_LOGO" == "1" ]; then
-    echo "convert resources/logo/0.png to drastic_logo_0.raw"
-    echo "convert resources/logo/1.png to drastic_logo_1.raw"
     ./png2raw
 fi
 
@@ -29,12 +37,10 @@ echo 10 > /proc/sys/vm/swappiness
 cd $mydir
 
 if [ "$CUST_CPUCLOCK" == "1" ]; then
-    echo "set customized cpuspeed"
-    cpuclock 1500
+    ./cpuclock 1500
 fi
 
 ./drastic "$1"
-
 sync
 
 echo $sv > /proc/sys/vm/swappiness
