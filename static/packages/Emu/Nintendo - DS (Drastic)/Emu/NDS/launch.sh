2f493a9536de6b3c02c6798a14104d984eeac04d
Schmurtz <Schmurtz@gmail.com>
2023-11-20 23:24:10 +0100

    Restore customized 1.6
    removed useless files
    removed duplicated libs which are already in Onion shrunken Parasyte
    cpuclock used from Onion bin files
    use only stop_audioserver.sh
    set "frameskip_type" to 2 instead of 4
    Use dedicated Onion icon
    Removed shortcuts splashscreen
    png2raw script section commented
    
    Todo :
    If required : a shortcut splashscreen with infopanel
    Optionally : modify the libs to support the native Exo font from Onion
    (our default fonts : https://onionui.github.io/docs/theme-design#default-fonts-credit-aemiii91
    Drastic SDL2 menu font : https://github.com/steward-fu/nds_miyoo/blob/d989f8b417760ee411887437442da4b45ec0bd41/sdl2/src/video/mmiyoo/SDL_video_mmiyoo.h#L82C61-L82C61)

--- static/packages/Emu/Nintendo - DS (Drastic)/Emu/NDS/launch.sh
+++ static/packages/Emu/Nintendo - DS (Drastic)/Emu/NDS/launch.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 mydir=`dirname "$0"`
 
-cd $mydir
-if [ ! -f "/tmp/.show_hotkeys" ]; then
-    touch /tmp/.show_hotkeys
-    LD_LIBRARY_PATH=./libs:/customer/lib:/config/lib ./show_hotkeys
-fi
-
 export HOME=$mydir
 export PATH=$mydir:$PATH
 export LD_LIBRARY_PATH=$mydir/libs:$LD_LIBRARY_PATH
@@ -14,20 +8,21 @@ export SDL_VIDEODRIVER=mmiyoo
 export SDL_AUDIODRIVER=mmiyoo
 export EGL_VIDEODRIVER=mmiyoo
 
-if [ -f /mnt/SDCARD/.tmp_update/script/stop_audioserver.sh ]; then
-    /mnt/SDCARD/.tmp_update/script/stop_audioserver.sh
-else
-    killall audioserver
-    killall audioserver.mod
-fi
 
-CUST_LOGO=0
+. /mnt/SDCARD/.tmp_update/script/stop_audioserver.sh
+
+CUST_LOGO=1
 CUST_CPUCLOCK=1
 
 cd $mydir
-if [ "$CUST_LOGO" == "1" ]; then
-    ./png2raw
-fi
+
+
+# Uncomment this section if you need to customize the Drastic menu logo :
+# if [ "$CUST_LOGO" == "1" ]; then
+    # echo "convert resources/logo/0.png to drastic_logo_0.raw"
+    # echo "convert resources/logo/1.png to drastic_logo_1.raw"
+    # ./png2raw
+# fi
 
 sv=`cat /proc/sys/vm/swappiness`
 
@@ -37,10 +32,12 @@ echo 10 > /proc/sys/vm/swappiness
 cd $mydir
 
 if [ "$CUST_CPUCLOCK" == "1" ]; then
-    ./cpuclock 1500
+    echo "set customized cpuspeed"
+    cpuclock 1500
 fi
 
 ./drastic "$1"
+
 sync
 
 echo $sv > /proc/sys/vm/swappiness
