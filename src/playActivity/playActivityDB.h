72335554755b34b9fb5f8cf0001f22e802003624
theUnBurn <patrick.a.hurtado@gmail.com>
2023-12-06 08:38:58 -0600

    Updating column counts after reducing one from the stmt

--- src/playActivity/playActivityDB.h
+++ src/playActivity/playActivityDB.h
@@ -208,14 +208,14 @@ PlayActivities *play_activity_find_all(void)
             rom->image_path = get_rom_image_path(rom->file_path);
         }
 
-        entry->play_count = sqlite3_column_int(stmt, 5);
-        entry->play_time_total = sqlite3_column_int(stmt, 6);
-        entry->play_time_average = sqlite3_column_int(stmt, 7);
+        entry->play_count = sqlite3_column_int(stmt, 4);
+        entry->play_time_total = sqlite3_column_int(stmt, 5);
+        entry->play_time_average = sqlite3_column_int(stmt, 6);
         if (sqlite3_column_text(stmt, 8) != NULL) {
-            entry->first_played_at = strdup((const char *)sqlite3_column_text(stmt, 8));
+            entry->first_played_at = strdup((const char *)sqlite3_column_text(stmt, 7));
         }
         if (sqlite3_column_text(stmt, 9) != NULL) {
-            entry->last_played_at = strdup((const char *)sqlite3_column_text(stmt, 9));
+            entry->last_played_at = strdup((const char *)sqlite3_column_text(stmt, 8));
         }
 
         play_activities->play_time_total += entry->play_time_total;
