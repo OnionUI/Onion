760cbb7c0aa913b7857bc1ae2dbe30c075836686
Schmurtz <Schmurtz@gmail.com>
2023-12-09 14:43:27 +0100

    PICO-8 configurations
    1 - Change "Roms/PICO/bbs" folder to "Roms/PICO/splore"
    2 - Disable the pico-8 logs (record_activity_log 0)
    3 - Rename the shortcut to "~Run PICO-8 with Splore"
    4 - Add a refresh pico roms after splore (when it's necessary)

--- static/packages/RApp/PICO-8 (PICO-8 standalone)/RApp/PICO-8/launch.sh
+++ static/packages/RApp/PICO-8 (PICO-8 standalone)/RApp/PICO-8/launch.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 echo $0 $*
-picodir=`dirname "$0"`
+picodir=$(dirname "$0")
 
 export picodir="$picodir"
 export picoconfig="/mnt/SDCARD/Saves/CurrentProfile/saves/PICO-8"
 export rompath="$1"
-export filename=`basename "$rompath"`
+export filename=$(basename "$rompath")
 
 cd $picodir
 export PATH=$PATH:$PWD/bin
 export HOME=$picoconfig
-export BBS_DIR="/mnt/SDCARD/Roms/PICO/bbs/" # change me to a location for the bbs carts to go to
+export BBS_DIR="/mnt/SDCARD/Roms/PICO/splore/" # change me to a location for the bbs carts to go to
 
 if [ ! -d "$BBS_DIR" ]; then
     mkdir -p "$BBS_DIR"
@@ -38,21 +38,21 @@ fixconfig() {
 
     for setting in window_size screen_size windowed window_position frameless fullscreen_method blit_method transform_screen; do
         case $setting in
-            window_size) new_value="$set_window_size" ;;
-            screen_size) new_value="$set_screen_size" ;;
-            windowed) new_value="$set_windowed" ;;
-            window_position) new_value="$set_window_position" ;;
-            frameless) new_value="$set_frameless" ;;
-            fullscreen_method) new_value="$set_fullscreen_method" ;;
-            blit_method) new_value="$set_blit_method" ;;
-            transform_screen) new_value="$set_transform_screen" ;;
+        window_size) new_value="$set_window_size" ;;
+        screen_size) new_value="$set_screen_size" ;;
+        windowed) new_value="$set_windowed" ;;
+        window_position) new_value="$set_window_position" ;;
+        frameless) new_value="$set_frameless" ;;
+        fullscreen_method) new_value="$set_fullscreen_method" ;;
+        blit_method) new_value="$set_blit_method" ;;
+        transform_screen) new_value="$set_transform_screen" ;;
         esac
 
         if grep -q "^$setting" "$config_file"; then
             sed -i "s/^$setting.*/$new_value/" "$config_file"
             echo "Updated setting: $setting"
         else
-            echo "$new_value" >> "$config_file"
+            echo "$new_value" >>"$config_file"
             echo "Added missing setting: $setting"
         fi
     done
@@ -61,9 +61,8 @@ fixconfig() {
     grep -E "window_size|screen_size|windowed|window_position|frameless|fullscreen_method|blit_method|transform_screen" "$config_file"
 }
 
-
 # when wifi is restarted, udhcpc and wpa_supplicant may be started with libpadsp.so preloaded, this is bad as they can hold mi_ao open even after audioserver has been killed.
-libpadspblocker() { 
+libpadspblocker() {
     wpa_pid=$(ps -e | grep "[w]pa_supplicant" | awk 'NR==1{print $1}')
     udhcpc_pid=$(ps -e | grep "[u]dhcpc" | awk 'NR==1{print $1}')
     if [ -n "$wpa_pid" ] && [ -n "$udhcpc_pid" ]; then
@@ -71,8 +70,8 @@ libpadspblocker() {
             echo "Network Checker: $wpa_pid(WPA) and $udhcpc_pid(UDHCPC) found preloaded with libpadsp.so"
             unset LD_PRELOAD
             killall -9 wpa_supplicant
-            killall -9 udhcpc 
-            $miyoodir/app/wpa_supplicant -B -D nl80211 -iwlan0 -c /appconfigs/wpa_supplicant.conf & 
+            killall -9 udhcpc
+            $miyoodir/app/wpa_supplicant -B -D nl80211 -iwlan0 -c /appconfigs/wpa_supplicant.conf &
             udhcpc -i wlan0 -s /etc/init.d/udhcpc.script &
             echo "Network Checker: Removing libpadsp.so preload on wpa_supp/udhcpc"
         fi
@@ -81,31 +80,37 @@ libpadspblocker() {
 
 start_pico() {
 
-	if [ ! -e "$picodir/bin/pico8_dyn" ]; then
-		infoPanel --title "PICO-8 binaries not found" --message "Native PICO-8 engine requires to purchase official \nbinaries which are not provided in Onion. \nGo to Lexaloffle's website, get Raspberry Pi version\n and copy \"pico8_dyn\" and \"pico8.dat\"\n to your SD card in \"/RApp/PICO-8/bin\"."
-		exit
-	fi
-	
+    if [ ! -e "$picodir/bin/pico8_dyn" ]; then
+        infoPanel --title "PICO-8 binaries not found" --message "Native PICO-8 engine requires to purchase official \nbinaries which are not provided in Onion. \nGo to Lexaloffle's website, get Raspberry Pi version\n and copy \"pico8_dyn\" and \"pico8.dat\"\n to your SD card in \"/RApp/PICO-8/bin\"."
+        exit
+    fi
+
     export LD_LIBRARY_PATH="$picodir/lib:$LD_LIBRARY_PATH"
     export SDL_VIDEODRIVER=mmiyoo
     export SDL_AUDIODRIVER=mmiyoo
     export EGL_VIDEODRIVER=mmiyoo
-	
+
     fixconfig
-    
+
     . /mnt/SDCARD/.tmp_update/script/stop_audioserver.sh
-    
+
     libpadspblocker
-    
-	if [ "$filename" = "~Run PICO-8.pico-8" ]; then 
-		LD_PRELOAD="$picodir/lib/libcpt_hook.so" pico8_dyn -splore -preblit_scale 3 -pixel_perfect 0
-	else
-		LD_PRELOAD="$picodir/lib/libcpt_hook.so" pico8_dyn -preblit_scale 3 -pixel_perfect 0 -run "$rompath"
-	fi
+
+    if [ "$filename" = "~Run PICO-8 with Splore.pico-8" ]; then
+        num_files_before=$(ls -1 "$BBS_DIR" | wc -l)
+        LD_PRELOAD="$picodir/lib/libcpt_hook.so" pico8_dyn -splore -preblit_scale 3 -pixel_perfect 0
+        num_files_after=$(ls -1 "$BBS_DIR" | wc -l)
+        if [ "$num_files_before" -ne "$num_files_after" ]; then
+            rm -f /mnt/SDCARD/Roms/PICO/PICO_cache6.db
+        fi
+
+    else
+        LD_PRELOAD="$picodir/lib/libcpt_hook.so" pico8_dyn -preblit_scale 3 -pixel_perfect 0 -run "$rompath"
+    fi
 }
 
 main() {
-    echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+    echo performance >/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
     start_pico
 }
 
