428f2f27ce24c57a435b62d2a7d37812d071dfe9
Schmurtz <schmurtz@gmail.com>
2023-12-11 22:30:35 +0100

    FEAT: multidisc playlist (.m3u) generator (#1301)
    Create playlists automatically from multidisk roms New script m3u_gen.sh
    (easy to adapt to GLO if necessary)
    Available from `Tweaks` -> `Tools` section.
    Additional folders added to cue generator.
    By default the script is systematically applied to `PS, SEGACD, NEOCD,
    PCE, PCFX, AMIGA` rom folders.
    
    The supported multidisc file names are numerous (not key sensitive and
    support "disc" or "disk"), for example :
    `MyGame (Disc 1).chd` or `MyGame (disc 1).chd` or `MyGame (DISC 1).chd`
    or `MyGame (Disk 1).chd`
    `MyGame (USA) (Disc 1).bin` + `MyGame (USA) (Disc 1).cue`
    `MyGame (USA) (Disc 1) (Rev 1).chd`
    `MyGame (Disc 1 of .2).chd`
    ...

--- src/tweaks/appstate.h
+++ src/tweaks/appstate.h
@@ -67,6 +67,7 @@ static List _menu_battery_percentage;
 static List _menu_advanced;
 static List _menu_reset_settings;
 static List _menu_tools;
+static List _menu_tools_m3uGenerator;
 static List _menu_diagnostics;
 
 void menu_free_all(void)
@@ -85,6 +86,7 @@ void menu_free_all(void)
     list_free(&_menu_advanced);
     list_free(&_menu_reset_settings);
     list_free(&_menu_tools);
+    list_free(&_menu_tools_m3uGenerator);
     list_free(&_menu_diagnostics);
 
     menu_icons_free_all();
