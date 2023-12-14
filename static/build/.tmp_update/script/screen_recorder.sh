7c95fe1d5396c90a9de965fdd14be105d500aeaa
Schmurtz <Schmurtz@gmail.com>
2023-12-14 20:17:14 +0100

    Add small vibration on recording start

--- static/build/.tmp_update/script/screen_recorder.sh
+++ static/build/.tmp_update/script/screen_recorder.sh
@@ -23,19 +23,19 @@ cd $re_dir
 
 # we'll hopefully never need this, but it checks whether /dev/l has the screen init already (and mi_disp exists, otherwise we can't change the screen colour)
 # the function will call, the binary will run but if it detects disp is init it will return
-check_disp_init() { 
+check_disp_init() {
     if [ -z "$sysdir" ] || [ ! -x "$sysdir/bin/disp_init" ]; then
         echo "Error: disp_init not found or not executable"
-        exit 
+        exit
     fi
-    
-    if ! pgrep -f "/dev/l" > /dev/null; then
+
+    if ! pgrep -f "/dev/l" >/dev/null; then
         $sysdir/bin/disp_init &
     fi
 }
 
 file_write() {
-    echo "$2" > "$1"
+    echo "$2" >"$1"
 }
 
 setup_gpio_for_vibration() {
@@ -52,6 +52,12 @@ vibrate() {
 }
 
 short_vibration() {
+    vibrate 1
+    usleep 15000
+    vibrate 0
+}
+
+strong_vibration() {
     vibrate 1
     usleep 100000
     vibrate 0
@@ -71,14 +77,12 @@ pulsating_vibration() {
 
     i=1
     while [ "$i" -le "$loop_count" ]; do
-        vibrate 1
-        usleep 15000
-        vibrate 0
+        short_vibration
         usleep "$usleep_duration"
         i=$((i + 1))
     done
 
-    short_vibration
+    strong_vibration
 }
 
 # use the screen to countdown
@@ -90,10 +94,10 @@ show_countdown() {
     if [ $# -gt 0 ] && echo "$1" | grep -qE '^[0-9]+$' && [ "$1" -ge 0 ] && [ "$1" -le 10 ]; then
         count="$1"
         echo $count
-    else 
+    else
         count=0
     fi
-    
+
     check_disp_init
     default_colour="128 128 128"
     pulse_colour="200 200 200"
@@ -102,18 +106,18 @@ show_countdown() {
         combinedRGB=$(cat "$sysdir/config/display/blueLightRGB")
         combinedRGB=$(echo "$combinedRGB" | tr -d '[:space:]/#')
 
-        blfB=$(( combinedRGB & 0xFF ))
-        blfG=$(( (combinedRGB >> 8) & 0xFF ))
-        blfR=$(( (combinedRGB >> 16) & 0xFF ))
+        blfB=$((combinedRGB & 0xFF))
+        blfG=$(((combinedRGB >> 8) & 0xFF))
+        blfR=$(((combinedRGB >> 16) & 0xFF))
         current_colour="$blfB $blfG $blfR"
     else
         current_colour=$default_colour
     fi
 
     original_colour=$current_colour
-    
+
     if [ "$count" -gt 1 ]; then
-        pulsating_vibration $(( $count * 2 )) &
+        pulsating_vibration $(($count * 2)) &
     else
         pulsating_vibration 1 &
     fi
@@ -124,7 +128,7 @@ show_countdown() {
             new_green=$(echo "$original_colour $pulse_colour" | awk -v step="$step" '{printf "%.0f", $2 + ($5 - $2) * step / 10}')
             new_red=$(echo "$original_colour $pulse_colour" | awk -v step="$step" '{printf "%.0f", $3 + ($6 - $3) * step / 10}')
 
-            echo "colortemp 0 0 0 0 $new_blue $new_green $new_red" > /proc/mi_modules/mi_disp/mi_disp0
+            echo "colortemp 0 0 0 0 $new_blue $new_green $new_red" >/proc/mi_modules/mi_disp/mi_disp0
             usleep 20000
         done
 
@@ -133,49 +137,50 @@ show_countdown() {
             new_green=$(echo "$pulse_colour $original_colour" | awk -v step="$step" '{printf "%.0f", $2 + ($5 - $2) * step / 10}')
             new_red=$(echo "$pulse_colour $original_colour" | awk -v step="$step" '{printf "%.0f", $3 + ($6 - $3) * step / 10}')
 
-            echo "colortemp 0 0 0 0 $new_blue $new_green $new_red" > /proc/mi_modules/mi_disp/mi_disp0
+            echo "colortemp 0 0 0 0 $new_blue $new_green $new_red" >/proc/mi_modules/mi_disp/mi_disp0
             usleep 20000
         done
     done
 }
 
-
 show_indicator() {
-    imgpop 10000 0 $rec_icon 150 435 > /dev/null 2>&1 &
+    imgpop 10000 0 $rec_icon 150 435 >/dev/null 2>&1 &
 }
 
 toggle_ffmpeg() {
     sync
-    if pgrep -f "ffmpeg -f fbdev -nostdin" > /dev/null; then
+    if pgrep -f "ffmpeg -f fbdev -nostdin" >/dev/null; then
         pkill -2 -f "ffmpeg -f fbdev -nostdin"
         killall -9 imgpop
-        
+
         if [ "$(cat "$sysdir/config/recCountdown")" -lt 1 ]; then
             show_countdown 0
         else
             show_countdown 1
         fi
-        
+
         rm -f "$active_file"
     else
         if [ "$(cat "$sysdir/config/recCountdown")" -gt 1 ]; then
             show_countdown $(cat "$sysdir/config/recCountdown")
+        else
+            short_vibration &
         fi
 
         if [ -f "$sysdir/config/.recIndicator" ]; then
             show_indicator
         fi
-        
-        echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-        
-        ffmpeg -f fbdev -nostdin -framerate 25 -i /dev/fb0 -vf "vflip,hflip, format=yuv420p" -c:v libx264 -preset ultrafast -tune zerolatency -maxrate 2000k -bufsize 6000k -threads 0 "$(date +%Y%m%d%H%M%S).mp4" > /dev/null 2>&1 &
-        
+
+        echo performance >/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+
+        ffmpeg -f fbdev -nostdin -framerate 25 -i /dev/fb0 -vf "vflip,hflip, format=yuv420p" -c:v libx264 -preset ultrafast -tune zerolatency -maxrate 2000k -bufsize 6000k -threads 0 "$(date +%Y%m%d%H%M%S).mp4" >/dev/null 2>&1 &
+
         sleep 0.5
-        
-        if ! pgrep -f "ffmpeg -f fbdev -nostdin" > /dev/null; then
+
+        if ! pgrep -f "ffmpeg -f fbdev -nostdin" >/dev/null; then
             return 1
         fi
-        
+
         touch "$active_file"
     fi
     rm -f "$lock_file"
@@ -184,15 +189,15 @@ toggle_ffmpeg() {
 
 hardkill_ffmpeg() {
     killall -2 ffmpeg # give it a chance
-    
+
     sleep 0.5
-    
-    if pgrep -f "ffmpeg -f fbdev -nostdin" > /dev/null; then
+
+    if pgrep -f "ffmpeg -f fbdev -nostdin" >/dev/null; then
         killall -9 ffmpeg
         rm -f "$lock_file"
         return 1
     fi
-   
+
     rm -f "$active_file"
     rm -f "$lock_file"
     killall -9 imgpop
@@ -200,18 +205,19 @@ hardkill_ffmpeg() {
 }
 
 case "$1" in
-    toggle)
-        toggle_ffmpeg
-        exit $?
-        ;;
-    hardkill)
-        hardkill_ffmpeg
-        exit $? 
-        ;;
-    *)
-        echo "Invalid argument. Usage: $0 {toggle|hardkill}"
-        rm -f "$lock_file"
-        exit 1
+toggle)
+    toggle_ffmpeg
+    exit $?
+    ;;
+hardkill)
+    hardkill_ffmpeg
+    exit $?
+    ;;
+*)
+    echo "Invalid argument. Usage: $0 {toggle|hardkill}"
+    rm -f "$lock_file"
+    exit 1
+    ;;
 esac
 
 rm -f "$lock_file"
