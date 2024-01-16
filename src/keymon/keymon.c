3b2e1e859efe6502182f21040f5292c4cde2dddb
Sichroteph <Christophe.Jeannette@gmail.com>
2024-01-16 15:14:52 +0100

    drastic keymon whitelist revertion

--- src/keymon/keymon.c
+++ src/keymon/keymon.c
@@ -117,8 +117,7 @@ int suspend(uint32_t mode)
                             (strcmp(comm, "(MainUI)")) &&
                             (strcmp(comm, "(tee)")) &&
                             (strncmp(comm, "(audioserver", 12)) &&
-                            (strcmp(comm, "(batmon)")) &&
-                            (strcmp(comm, "(drastic)"))) {
+                            (strcmp(comm, "(batmon)"))){
                             kill(pid, (mode == 1) ? SIGTERM : SIGKILL);
                             ret++;
                         }
