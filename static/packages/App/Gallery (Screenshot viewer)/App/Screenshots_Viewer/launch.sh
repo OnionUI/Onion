575a70dc34b586a196e9434a4cc8ba1a21ca2b7b
tGecko <tGecko@proton.me>
2024-02-24 16:24:26 +0100

    Fix infoPanel crashes when loading incompatible images, that also need to be stretched (#1418)
    Closes #1386 
    
    The images that crashed the infoPanel were in a different format, which
    SDL_SoftStretch couldn't handle, leaving image_to_draw empty and
    subsequently crashing when drawing the empty image.
    To safeguard against this, create a separate surface and check the
    return of SoftStretch for error.
    
    Testing this showed that some of the stretched images looked horrible,
    so now only stretch images if they are too big to fit the screen. (MMv4
    752x560 screenshots mainly)
    
    Additionally, add some error handling to the Gallery launch script, so
    it would show "Error" instead of "No screenshots found" when infoPanel
    crashes to be less misleading.
    
    To test, copy the screenshot folder from the issue #1386 to your
    SD/Screenshots and confirm it doesn't crash when loading the different
    screenshots.
    
    ---------
    
    Co-authored-by: XK <47260768+XK9274@users.noreply.github.com>
    Co-authored-by: Aemiii91 <44569252+Aemiii91@users.noreply.github.com>

--- static/packages/App/Gallery (Screenshot viewer)/App/Screenshots_Viewer/launch.sh
+++ static/packages/App/Gallery (Screenshot viewer)/App/Screenshots_Viewer/launch.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 echo ":: Gallery - Screenshots Viewer ::"
 
-/mnt/SDCARD/.tmp_update/bin/infoPanel -d /mnt/SDCARD/Screenshots --show-theme-controls
+infoPanel -d /mnt/SDCARD/Screenshots --show-theme-controls
 ec=$?
 
 # cancel or success from infoPanel
-if [ $ec -eq 255 ] || [ $ec -eq 0 ] ; then
+if [ $ec -eq 255 ] || [ $ec -eq 0 ]; then
     exit 0
+elif [ $ec -eq 1 ]; then
+    infoPanel -t Gallery -m "No screenshots found"
+else
+    # something went wrong
+    infoPanel -t Gallery -m "An error occurred - code: $ec"
 fi
-
-/mnt/SDCARD/.tmp_update/bin/infoPanel -t Gallery -m "No screenshots found"
