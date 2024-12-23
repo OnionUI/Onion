80a5596f19481dd7e837e6017ccf10910ba6aab0
Aemiii91 <44569252+Aemiii91@users.noreply.github.com>
2024-12-23 23:14:03 +0100

    Add gpSP save migration + restore mGBA transfer option

--- static/packages/Emu/Nintendo - GBA (gpSP)/Emu/GBA/launch.sh
+++ static/packages/Emu/Nintendo - GBA (gpSP)/Emu/GBA/launch.sh
@@ -18,9 +18,37 @@ mgba_save="$savedir/mGBA/$romname.srm"
 default_core=gpsp
 
 # check if gpSP save states exist
-if [ ! -f "$gpsp_state" ] && [ ! -f "$gpsp_save" ] && [ [ -f "$mgba_state" ] || [ -f "$mgba_save" ] ]; then
-    default_core=mgba
+if [ ! -f "$gpsp_state" ] && [ ! -f "$gpsp_save" ]; then
+    if [ -f "$mgba_save" ]; then
+        LD_PRELOAD=/mnt/SDCARD/miyoo/lib/libpadsp.so /mnt/SDCARD/.tmp_update/bin/prompt -r \
+            -t "GBA CORE CHANGED" \
+            -m "Default GBA core is now gpSP.\nDo you want to transfer your\nmGBA save file for this game?" \
+            "Transfer save to gpSP" \
+            "Keep playing with mGBA"
 
+        retcode=$?
+
+        if [ $retcode -eq 0 ]; then
+            # remove weird gpSP file if it exists
+            if [ -f "$savedir/gpSP" ]; then
+                rm -f "$savedir/gpSP"
+            fi
+
+            # create gpSP save directory
+            mkdir -p "$savedir/gpSP"
+
+            # transfer save file
+            cp "$mgba_save" "$gpsp_save"
+        else
+            default_core=mgba
+        fi
+    elif [ -f "$mgba_state" ]; then
+        default_core=mgba
+    fi
+fi
+
+# if core is mgba, create a config file
+if [ "$default_core" = "mgba" ]; then
     mkdir -p "$homedir/.game_config"
     echo "core = \"mgba_libretro\"" > "$romcfgpath"
 fi
