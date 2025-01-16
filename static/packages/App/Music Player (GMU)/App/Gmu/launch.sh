1975e9c08674c46459f37581e9acbe05e2790b56
Aemiii91 <44569252+Aemiii91@users.noreply.github.com>
2025-01-16 01:13:41 +0100

    GMU: Keep the music playing when turning off display (#1749)
    GMU will now keep playing music when the screen is off. And the
    system-wide sleep timer will be disabled when using GMU.
    
    ## Changes
    
    - Added `stay_awake` flag when launching GMU
    - Changed `keymon` to not suspend and mute when `stay_awake` is enabled
    
    Closes #1717

--- static/packages/App/Music Player (GMU)/App/Gmu/launch.sh
+++ static/packages/App/Music Player (GMU)/App/Gmu/launch.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-my_dir=`dirname $0`
+my_dir=$(dirname $0)
 cd $my_dir
 
 echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 . /mnt/SDCARD/.tmp_update/script/stop_audioserver.sh
+
+touch /tmp/stay_awake
 
 HOME=/mnt/SDCARD/App/Gmu
 LD_LIBRARY_PATH=/mnt/SDCARD/.tmp_update/lib/parasyte:./lib:$LD_LIBRARY_PATH
@@ -11,3 +13,4 @@ export LD_PRELOAD=./lib/libSDL-1.2.so.0
 ./gmu.bin -c gmu.miyoo.conf
 unset LD_PRELOAD
 
+rm -f /tmp/stay_awake
