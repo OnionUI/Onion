2f236a62d24f83ee3c962b2b9e190d3a34cd5ff5
Blitz <jboss38@users.noreply.github.com>
2026-01-17 21:34:47 +0100

    Adding support of Pokemon trading / combat for GBA (#1790)
    Hello,
    
    This pull request is made for testing purpose for the support of the
    Pokemon trading and combat for GBA Red FIRE and Green LEAF only.
    
    You can trade pokemon between RED to RED and GREEN to GREEN but ALSO
    between RED and GREEN, which currently not possible with gpSP only.
    
    So you can find script here, there is A LOT of cleanup todo in the
    script but it's working if you want to use it.
    (The script is based on this one
    https://github.com/OnionUI/Onion/pull/1007)
    
    
    **How To Install It  :**
    
    - Download the scripts and then put on your SDCARD to this path
    
    - ADD :
    ".tmp_update/script/netplay/easy-netplay_client_pokemon_GBA.sh"
    ".tmp_update/script/netplay/easy-netplay_server_pokemon_GBA.sh"
    
    - Replace:
     "App/romscripts/Netplay.sh"
    
    - Be sure to make a BACKUP of the original App/romscripts/Netplay.sh
    
    **How To Run It  :**
    - First backup everything
    - Install the script of this commits
    - Be sure that you have a made a INGAME save of Pokemon GBA before with
    gpSP Game core.
    - On the host: Head to the rom you wish to play, RED or Green and press
    Y to open GLO (Also in Recents/favs etc)
    - On the host: Select "Netplay (core supported: gpsp)"
    - On the host: Select "Host a session"
    - On the host: Select "Easy Netplay - Pokemon Trade/Battle GBA" - It
    will now wait for a client
    - On the client: Head to the rom you wish to play, RED or Green and
    press Y to open GLO (Also in Recents/favs etc)
    - On the client: Select "Netplay (core supported: gpsp)"
    - On the client: Select "Join a session"
    - On the client: Select "Easy Netplay - Pokemon Trade/Battle GBA" - It
    will now look for a host
    (PS : Sometime client and host don't synchronise, for better result
    start "looking for the host" on the Client 1 second before "running the
    server" on the host)
    - Wait for the process to complete
    - Select Start on the splash screens - Then choose your last Save INGAME
    
    **For the trade**
    - Go to the Pokemon center and head to the trade link with the WORLD
    - Go through the dialogue until she allows you into the room
    - Trade your Pokemon or do the Battle.
    - When done, Go out of the door and do a INGAME save, on both device
    - Important: Retroarch must exit gracefully without the script dying
    with it. - Press Menu + Select and then B, then Exit.
    - Important: Do not interrupt this process, you should see cleanup
    changes
    - Start the game in single player mode (Just click it from recents/fav
    etc) and it should load your save.
    - Observe in both games you have now traded Pokemon.
    
    
    Note : this process generate backup for the concernade game SAVES FILES
    and also your STATES FILES
    
    
    Have fun !
    
    ---------
    
    Co-authored-by: Matt W <47260768+XK9274@users.noreply.github.com>

---
+++ static/build/.tmp_update/script/netplay/easy-netplay_client_pokemon_GBA.sh
@@ -0,0 +1,246 @@
+# GLO Pokemon trade
+# Used within GLO as an addon script.
+
+# Env setup
+sysdir=/mnt/SDCARD/.tmp_update
+miyoodir=/mnt/SDCARD/miyoo
+LD_LIBRARY_PATH="/lib:/config/lib:$miyoodir/lib:$sysdir/lib:$sysdir/lib/parasyte"
+hostip="192.168.100.100" # This should be the default unless the user has changed it..
+rm /tmp/stop_now
+client_rom="$1"
+romdirname=$(echo "$client_rom" | grep -o '/Roms/[^/]*' | cut -d'/' -f3)
+client_rom_filename=$(basename "$client_rom")
+client_rom_filename_NoExt="${client_rom_filename%.*}"
+netplaycore="/mnt/SDCARD/RetroArch/.retroarch/cores/gpsp_libretro.so"
+
+## Source global utils
+logfile=pokemon_link
+. $sysdir/script/log.sh
+
+log "-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* Easy Netplay Pokemon Client GBA -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
+
+##########
+##Setup.##
+##########
+
+# Wait for the host to tell us it's ready, this happens just before it starts its RA session and we look in /tmp for a file indicator (file removed in host script cleanup)
+wait_for_host() {
+
+    local counter=0
+
+    build_infoPanel_and_log "Ready" "Waiting for host to ready up"
+    while true; do
+        sync
+        check_stop
+        for file in /tmp/host_ready; do
+            if [ -f "$file" ]; then
+                build_infoPanel_and_log "Message from host" "Setup complete"
+                rm /tmp/host_ready # be ready for the second use of host_ready flag
+                break 2
+            fi
+        done
+
+        sleep 1
+        counter=$((counter + 1))
+
+        if [ $counter -ge 25 ]; then
+            build_infoPanel_and_log "Error" "The host didn't ready up, cannot continue..."
+            notify_stop
+        fi
+    done
+}
+
+# Start retroarch with -C in client mode if everything's gone to plan
+start_retroarch() {
+    build_infoPanel_and_log "Starting RA" "Starting RetroArch"
+
+    log "\n############################ RETROARCH DEBUGGING ############################"
+    log "client_rom (local): ${client_rom}"
+    log "core: $netplaycore"
+    log "core_config_folder: $core_config_folder"
+    log "cpuspeed: $cpuspeed"
+    log "hostip: $hostip"
+    log "###############################################################################"
+
+    if [ -n "$cpuspeed" ]; then
+        log "We set core CPU speed for Netplay: $cpuspeed"
+        PreviousCPUspeed=$(cat "/mnt/SDCARD/Saves/CurrentProfile/config/${core_config_folder}/cpuclock.txt")
+        echo -n $cpuspeed >"/mnt/SDCARD/Saves/CurrentProfile/config/${core_config_folder}/cpuclock.txt"
+    fi
+
+    wait_for_host # we wait a second flag triggered from the host once RA is running
+    cd /mnt/SDCARD/RetroArch
+    log "Starting RetroArch loaded with $client_rom"
+    HOME=/mnt/SDCARD/RetroArch ./retroarch --appendconfig=./.retroarch/easynetplay_override.cfg -C $hostip -L "$netplaycore" "$client_rom"
+
+    if [ -n "$PreviousCPUspeed" ]; then
+        log "We restore previous core CPU speed: $PreviousCPUspeed"
+        echo -n $PreviousCPUspeed >"/mnt/SDCARD/Saves/CurrentProfile/config/${core_config_folder}/cpuclock.txt"
+    else
+        rm -f "/mnt/SDCARD/Saves/CurrentProfile/config/${core_config_folder}/cpuclock.txt"
+    fi
+}
+
+cleanup() {
+    build_infoPanel_and_log "Cleanup" "Cleaning up after Pokemon session\n Do not power off!"
+
+    pkill -9 pressMenu2Kill
+
+    if is_running infoPanel; then
+        killall -9 infoPanel
+    fi
+
+    # restore_wifi_state
+    . "$sysdir/script/network/hotspot_cleanup.sh"
+    restore_ftp
+
+    # Remove some files we prepared and received
+    log "Removing stale files"
+    rm "/tmp/host_ready"
+    rm "/tmp/stop_now"
+    rm "/tmp/wpa_supplicant.conf_bk"
+    rm "/tmp/dismiss_info_panel"
+    sync
+
+    log "Cleanup done"
+
+    exit
+}
+
+###########
+#Utilities#
+###########
+
+# Use the safe word
+notify_stop() {
+    notify_peer "stop_now"
+    sleep 2
+    cleanup
+}
+
+# Check stop, if the client tells us to stop we will.
+check_stop() {
+    sync
+    if [ -e "/tmp/stop_now" ]; then
+        build_infoPanel_and_log "Message from client" "The host has had a problem setting up the session"
+        sleep 2
+        cleanup
+    fi
+}
+
+# Notify other MMP
+notify_peer() {
+    local notify_file="/tmp/$1"
+    touch "$notify_file"
+    sync
+    curl -T "$notify_file" "ftp://${hostip}/${notify_file}" >/dev/null 2>&1 # the first / after the IP must be not encoded
+
+    if [ $? -eq 0 ]; then
+        log "Successfully transferred $notify_file to ftp://${hostip}/${notify_file}"
+    else
+        log "Failed to transfer $notify_file to ftp://${hostip}/${notify_file}"
+    fi
+}
+
+# We'll need FTP to transfer files
+start_ftp() {
+    if is_running bftpd; then
+        log "FTP already running, killing to rebind"
+        bftpd_p=$(ps | grep bftpd | grep -v grep | awk '{for(i=4;i<=NF;++i) printf $i" "}')
+        killall -9 bftpd
+        killall -9 tcpsvd
+        tcpsvd -E 0.0.0.0 21 ftpd -w / &
+    else
+        tcpsvd -E 0.0.0.0 21 ftpd -w / &
+        log "Starting FTP server"
+    fi
+}
+
+# This will restore the users original ftp state
+restore_ftp() {
+    log "Restoring original FTP server"
+    killall -9 tcpsvd
+    if flag_enabled ftpState; then
+        if flag_enabled authftpState; then
+            bftpd -d -c /mnt/SDCARD/.tmp_update/config/bftpdauth.conf &
+        else
+            bftpd -d -c /mnt/SDCARD/.tmp_update/config/bftpd.conf &
+        fi
+    fi
+}
+
+flag_enabled() {
+    flag="$1"
+    [ -f "$sysdir/config/.$flag" ]
+}
+
+build_infoPanel_and_log() {
+	local title="$1"
+	local message="$2"
+
+	log "Info Panel: \n\tStage: $title\n\tMessage: $message"
+	if is_running infoPanel; then
+		killall -9 infoPanel
+	fi
+	infoPanel --title "$title" --message "$message" --persistent &
+	sync
+	touch /tmp/dismiss_info_panel
+	sync
+	sleep 0.3
+	sync
+}
+
+confirm_join_panel() {
+    local title="$1"
+    local message="$2"
+
+    if [ "$title" = "Join now?" ]; then
+        if [ -e "/mnt/SDCARD/Roms/$romdirname/Imgs/$client_rom_filename_NoExt.png" ]; then # local rom image
+            pngScale "/mnt/SDCARD/Roms/$romdirname/Imgs/$client_rom_filename_NoExt.png" "/tmp/CurrentNetplay2.png" 100 100
+            sync
+            imgpop 2 1 "/tmp/CurrentNetplay2.png" 10 10 >/dev/null 2>&1 &
+        fi
+    fi
+
+    # TO DO : allow to confirm only once the host has started
+    infoPanel -t "$title" -m "$message"
+    retcode=$?
+
+    if [ $retcode -ne 0 ]; then
+        build_infoPanel_and_log "Cancelled" "User cancelled, exiting."
+        cleanup
+        exit 1
+    fi
+}
+
+stripped_game_names() {
+    client_rom_trimmed="$(echo "$client_rom_filename_NoExt" | sed -e 's/ ([^()]*)//g' -e 's/ [[A-z0-9!+]*]//g' -e 's/([^()]*)//g' -e 's/[[A-z0-9!+]*]//g')"
+    game_name_client="Client (me): \n$client_rom_trimmed"
+}
+
+is_running() {
+    process_name="$1"
+    pgrep "$process_name" >/dev/null
+}
+
+#########
+##Main.##
+#########
+
+lets_go() {
+    pressMenu2Kill $(basename $0) &
+    if [ -z "$client_rom" ]; then
+        build_infoPanel_and_log "Error" "No ROM path provided."
+        exit 1
+    fi
+    . "$sysdir/script/network/hotspot_join.sh"
+    start_ftp
+    stripped_game_names
+    wait_for_host
+    confirm_join_panel "Join now?" "Start the game on the host first! \n $game_name_client"
+    pkill -9 pressMenu2Kill
+    start_retroarch
+    cleanup
+}
+
+lets_go
